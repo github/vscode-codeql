d53b2f22ac0871180771f9dd745ae75b69907eb5
Dave Bartolomeo <dave@semmle.com>
2019-07-25 10:39:42 -0700

    Fix LGTM alerts

--- tools/build-tasks/gulpfile.js/index.js
+++ tools/build-tasks/gulpfile.js/index.js
@@ -1,7 +1,6 @@
 'use strict';
 
 require('ts-node').register({});
-const gulp = require('gulp');
 const { compileTypeScript, watchTypeScript } = require('../src/index');
 
 exports.default = compileTypeScript;
