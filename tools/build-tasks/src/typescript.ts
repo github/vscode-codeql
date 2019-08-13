b323621a2eaa86625666066140c13818b6dbdaf0
Dave Bartolomeo <dave@semmle.com>
2019-08-13 12:32:16 -0700

    Fix failure to load extension
    The extension was failing to load when installed via .vsix, but worked when run under the debugger. The issue was that `ql-vscode` had `fs-extra` as a `devDependency`, but used it at runtime. When launched under the debugger, the extension is run from the original source directory, where `node_modules` includes dev dependencies. When we package the extension into a .vsix, we only include production dependencies.
    
    I fixed the dependency, and then updated `launch.json` so that we now run the extension from the deployment directory instead of the source directory, so we get exactly the same `node_modules` that we would if installed via .vsix. This required a bit of fiddling with source map paths to make debugging still allow source-level breakpoints and stepping.

--- tools/build-tasks/src/typescript.ts
+++ tools/build-tasks/src/typescript.ts
@@ -3,6 +3,7 @@ import * as gulp from 'gulp';
 import * as path from 'path';
 import * as sourcemaps from 'gulp-sourcemaps';
 import * as ts from 'gulp-typescript';
+import { RushConfiguration } from '@microsoft/rush-lib';
 
 function goodReporter(): ts.reporter.Reporter {
   return {
@@ -22,13 +23,32 @@ function goodReporter(): ts.reporter.Reporter {
 const tsProject = ts.createProject('tsconfig.json');
 
 export function compileTypeScript() {
+  // Find this project's relative directory. Rush already knows this, so just ask.
+  const packageDir = path.resolve('.');
+  const rushConfig = RushConfiguration.loadFromDefaultLocation({
+    startingFolder: packageDir
+  });
+  const project = rushConfig.tryGetProjectForPath(packageDir);
+  if (!project) {
+    console.error(`Unable to find project for '${packageDir}' in 'rush.json'.`);
+    throw Error();
+  }
+
   return tsProject.src()
     .pipe(sourcemaps.init())
     .pipe(tsProject(goodReporter()))
     .pipe(sourcemaps.mapSources((sourcePath, file) => {
-      return path.join('a/b', sourcePath);
+      // The source path is kind of odd, because it's relative to the `tsconfig.json` file in the
+      // `typescript-config` package, which lives in the `node_modules` directory of the package
+      // that is being built. It starts out as something like '../../../src/foo.ts', and we need to
+      // strip out the leading '../../../'.
+      return path.join('a/b/c', sourcePath);
     }))
-    .pipe(sourcemaps.write('.', { includeContent: false, sourceRoot: '' }))
+    .pipe(sourcemaps.write('.', {
+      includeContent: false,
+      // Since the generated .js files are deployed to 'dist/<package>/out'
+      sourceRoot: path.join('../../..', project.projectRelativeFolder)
+    }))
     .pipe(gulp.dest('out'));
 }
 
