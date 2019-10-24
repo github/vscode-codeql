69b2c40979299c7538c1fd99a3a84bae5679d470
Henry Mercer <henry@semmle.com>
2019-10-24 17:03:42 +0100

    Add column sorting UI

--- extensions/ql-vscode/src/view/raw-results-table.tsx
+++ extensions/ql-vscode/src/view/raw-results-table.tsx
@@ -2,15 +2,21 @@ import cx from 'classnames';
 import * as React from "react";
 import { className, evenRowClassName, oddRowClassName, renderLocation, ResultTableProps, selectedClassName } from "./result-table-utils";
 import { RawTableResultSet, ResultValue } from "./results";
+import { assertNever } from "../helpers-pure";
 
 export type RawTableProps = ResultTableProps & { resultSet: RawTableResultSet };
 
+export interface RawTableState {
+  sortState?: SortState;
+}
 
-
-
-export class RawTable extends React.Component<RawTableProps, {}> {
+export class RawTable extends React.Component<RawTableProps, RawTableState> {
   constructor(props: RawTableProps) {
     super(props);
+
+    this.state = {
+      sortState: undefined
+    };
   }
 
   render(): React.ReactNode {
@@ -28,7 +34,8 @@ export class RawTable extends React.Component<RawTableProps, {}> {
               <th key={-1}><b>#</b></th>,
               ...resultSet.schema.columns.map((col, index) => {
                 const displayName = col.name || `[${index}]`;
-                return <th key={index}><b>{displayName}</b></th>;
+                const sortDirection = this.state.sortState && index === this.state.sortState.columnIndex ? this.state.sortState.direction : undefined;
+                return <th className={"sort-" + (sortDirection !== undefined ? sorting.Direction[sortDirection] : "none")} key={index} onClick={() => this.toggleSortStateForColumn(index)}><b>{displayName}</b></th>;
               })
             ]
           }
@@ -55,6 +62,24 @@ export class RawTable extends React.Component<RawTableProps, {}> {
       </tbody>
     </table>;
   }
+
+  private toggleSortStateForColumn(index: number) {
+    this.setState(previousState => {
+      const prevDirection = previousState.sortState && previousState.sortState.columnIndex === index ?
+        previousState.sortState.direction : undefined;
+      const nextDirection = sorting.nextDirection(prevDirection);
+      if (nextDirection === undefined) {
+        return { sortState: undefined };
+      } else {
+        return {
+          sortState: {
+            columnIndex: index,
+            direction: nextDirection
+          }
+        };
+      }
+    });
+  }
 }
 
 
@@ -62,14 +87,37 @@ export class RawTable extends React.Component<RawTableProps, {}> {
  * Render one column of a tuple.
  */
 function renderTupleValue(v: ResultValue, databaseUri: string): JSX.Element {
-    if (typeof v === 'string') {
-      return <span>{v}</span>
-    }
-    else if ('uri' in v) {
-      return <a href={v.uri}>{v.uri}</a>;
-    }
-    else {
-      return renderLocation(v.location, v.label, databaseUri);
+  if (typeof v === 'string') {
+    return <span>{v}</span>
+  }
+  else if ('uri' in v) {
+    return <a href={v.uri}>{v.uri}</a>;
+  }
+  else {
+    return renderLocation(v.location, v.label, databaseUri);
+  }
+}
+
+namespace sorting {
+  export enum Direction {
+    asc, desc
+  }
+
+  export function nextDirection(direction: Direction | undefined): Direction | undefined {
+    switch (direction) {
+      case Direction.asc:
+        return Direction.desc;
+      case Direction.desc:
+        return undefined;
+      case undefined:
+        return Direction.asc;
+      default:
+        return assertNever(direction);
     }
   }
-  
+}
+
+export interface SortState {
+  columnIndex: number;
+  direction: sorting.Direction;
+}
