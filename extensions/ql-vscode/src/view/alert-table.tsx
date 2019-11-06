a76767096da4e6f15c340d93f004c1a9a33a36b8
Don Okuda <don.okuda@gmail.com>
2019-11-06 10:50:34 -0800

    Fix table spacing when mixing different paths / non-path results
    Also get the info icon to show

--- extensions/ql-vscode/src/view/alert-table.tsx
+++ extensions/ql-vscode/src/view/alert-table.tsx
@@ -178,10 +178,10 @@ export class PathTable extends React.Component<PathTableProps, PathTableState> {
     const octiconChevronRight = <svg className="octicon" width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
     <path d="M5.7 13.7L5 13l4.6-4.6L5 3.7l.7-.7 5 5v.7l-5 5z" />
 </svg>;
-    const octiconListUnordered = <svg className="octicon" width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+    const octiconListUnordered = <svg className="octicon octicon-light" width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
     <path fill-rule="evenodd" clip-rule="evenodd" d="M2 3H1v1h1V3zm0 3H1v1h1V6zM1 9h1v1H1V9zm1 3H1v1h1v-1zm2-9h11v1H4V3zm11 3H4v1h11V6zM4 9h11v1H4V9zm11 3H4v1h11v-1z" />
 </svg>;
-    const octiconInfo = <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+    const octiconInfo = <svg className="octicon octicon-light" width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
     <path fill-rule="evenodd" clip-rule="evenodd" d="M8.568 1.03a6.8 6.8 0 0 1 4.192 2.02 7.06 7.06 0 0 1 .46 9.39 6.85 6.85 0 0 1-8.58 1.74 7 7 0 0 1-3.12-3.5 7.12 7.12 0 0 1-.23-4.71 7 7 0 0 1 2.77-3.79 6.8 6.8 0 0 1 4.508-1.15zm.472 12.85a5.89 5.89 0 0 0 3.41-2.07 6.07 6.07 0 0 0-.4-8.06 5.82 5.82 0 0 0-7.43-.74 6.06 6.06 0 0 0 .5 10.29 5.81 5.81 0 0 0 3.92.58zM8.51 7h-1v4h1V7zm0-2h-1v1h1V5z" />
 </svg>;
 
@@ -201,14 +201,16 @@ export class PathTable extends React.Component<PathTableProps, PathTableState> {
       const currentResultExpanded = this.state.expanded[expansionIndex];
       const indicator = currentResultExpanded ? octiconChevronDown : octiconChevronRight;
       if (result.codeFlows === undefined) {
-        let rowHeader = <td>{octiconInfo}</td>;
+        let rowHeader = <span></span>;
         if (result.locations !== undefined && result.locations.length > 0) {
-          rowHeader = <td>{renderSarifLocation('Result', result.locations[0])}</td>;
+          rowHeader = <span>{renderSarifLocation('Result', result.locations[0])}: </span>;
         }
         rows.push(
           <tr className={(resultIndex % 2) ? oddRowClassName : evenRowClassName}>
-            <td>{rowHeader}</td>
-            <td colSpan={3}>{msg}</td>
+            <td className="vscode-codeql__icon-cell">{octiconInfo}</td>
+            <td colSpan={4}>
+              {rowHeader}{msg}
+            </td>
           </tr>
         );
       }
