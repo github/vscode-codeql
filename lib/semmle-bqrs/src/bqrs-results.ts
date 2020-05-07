f61be465ec5f4215cb8000d005abc34559d1a4c3
Andrew Eisenberg <andrew.eisenberg@gmail.com>
2020-05-07 12:45:40 -0700

    Extract BQRS locations from string results

--- lib/semmle-bqrs/src/bqrs-results.ts
+++ lib/semmle-bqrs/src/bqrs-results.ts
@@ -1,4 +1,4 @@
-import { LocationStyle } from './bqrs-schema';
+import { LocationStyle } from "./bqrs-schema";
 
 // See https://help.semmle.com/QL/learn-ql/ql/locations.html for how these are used.
 export interface FivePartLocation {
@@ -31,52 +31,67 @@ export type LocationValue = RawLocationValue | WholeFileLocation;
 /** A location that may be resolved to a source code element. */
 export type ResolvableLocationValue = FivePartLocation | WholeFileLocation;
 
-
 /**
  * The CodeQL filesystem libraries use this pattern in `getURL()` predicates
  * to describe the location of an entire filesystem resource.
  * Such locations appear as `StringLocation`s instead of `FivePartLocation`s.
- * 
+ *
  * Folder resources also get similar URLs, but with the `folder` scheme.
  * They are deliberately ignored here, since there is no suitable location to show the user.
  */
-const WHOLE_FILE_LOCATION_REGEX = /file:\/\/(.+):0:0:0:0/;
-
+const FILE_LOCATION_REGEX = /file:\/\/(.+):([0-9]+):([0-9]+):([0-9]+):([0-9]+)/;
 /**
  * Gets a resolvable source file location for the specified `LocationValue`, if possible.
  * @param loc The location to test.
  */
-export function tryGetResolvableLocation(loc: LocationValue | undefined): ResolvableLocationValue | undefined {
+export function tryGetResolvableLocation(
+  loc: LocationValue | undefined
+): ResolvableLocationValue | undefined {
   if (loc === undefined) {
     return undefined;
-  }
-  else if ((loc.t === LocationStyle.FivePart) && loc.file) {
+  } else if (loc.t === LocationStyle.FivePart && loc.file) {
     return loc;
-  }
-  else if ((loc.t === LocationStyle.WholeFile) && loc.file) {
+  } else if (loc.t === LocationStyle.WholeFile && loc.file) {
     return loc;
-  }
-  else if ((loc.t === LocationStyle.String) && loc.loc) {
-    return tryGetWholeFileLocation(loc);
-  }
-  else {
+  } else if (loc.t === LocationStyle.String && loc.loc) {
+    return tryGetLocationFromString(loc);
+  } else {
     return undefined;
   }
 }
 
-export function tryGetWholeFileLocation(loc: StringLocation): WholeFileLocation | undefined {
-  const matches = WHOLE_FILE_LOCATION_REGEX.exec(loc.loc);
+export function tryGetLocationFromString(
+  loc: StringLocation
+): ResolvableLocationValue | undefined {
+  const matches = FILE_LOCATION_REGEX.exec(loc.loc);
   if (matches && matches.length > 1 && matches[1]) {
-    // Whole-file location.
-    // We could represent this as a FivePartLocation with all numeric fields set to zero,
-    // but that would be a deliberate misuse as those fields are intended to be 1-based.
-    return {
-      t: LocationStyle.WholeFile,
-      file: matches[1]
-    };
+    if (isWholeFileMatch(matches)) {
+      return {
+        t: LocationStyle.WholeFile,
+        file: matches[1],
+      };
+    } else {
+      return {
+        t: LocationStyle.FivePart,
+        file: matches[1],
+        lineStart: Number(matches[2]),
+        colStart: Number(matches[3]),
+        lineEnd: Number(matches[4]),
+        colEnd: Number(matches[5]),
+      }
+    }
   } else {
     return undefined;
   }
+}
+
+function isWholeFileMatch(matches: RegExpExecArray): boolean {
+  return (
+    matches[2] === "0" &&
+    matches[3] === "0" &&
+    matches[4] === "0" &&
+    matches[5] === "0"
+  );
 }
 
 export interface ElementBase {
@@ -93,8 +108,7 @@ export interface ElementWithLocation extends ElementBase {
   location: LocationValue;
 }
 
-export interface Element extends Required<ElementBase> {
-}
+export interface Element extends Required<ElementBase> {}
 
 export type PrimitiveColumnValue = string | boolean | number | Date;
 export type ColumnValue = PrimitiveColumnValue | ElementBase;
