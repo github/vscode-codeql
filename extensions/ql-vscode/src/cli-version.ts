bba2f0217b364c7eb9c0467c89566fd8f75fa85d
Andrew Eisenberg <aeisenberg@github.com>
2020-06-11 15:18:05 -0700

    Force undefined version if version command fails
    The assumption is that the cli is old or corrupted.
    In either case we want to upgrade.

--- extensions/ql-vscode/src/cli-version.ts
+++ extensions/ql-vscode/src/cli-version.ts
@@ -6,12 +6,19 @@ import { Logger } from "./logging";
  * Get the version of a CodeQL CLI.
  */
 export async function getCodeQlCliVersion(codeQlPath: string, logger: Logger): Promise<semver.SemVer | undefined> {
-  const output: string = await runCodeQlCliCommand(
-    codeQlPath,
-    ["version"],
-    ["--format=terse"],
-    "Checking CodeQL version",
-    logger
-  );
-  return semver.parse(output.trim()) || undefined;
+  try {
+    const output: string = await runCodeQlCliCommand(
+      codeQlPath,
+      ["version"],
+      ["--format=terse"],
+      "Checking CodeQL version",
+      logger
+    );
+    return semver.parse(output.trim()) || undefined;
+  } catch (e) {
+    // Failed to run the version command. This might happen if the cli version is _really_ old, or it is corrupted.
+    // Either way, we can't determine compatibility.
+    logger.log(`Failed to run 'codeql version'. Reason: ${e.message}`);
+    return undefined;
+  }
 }
