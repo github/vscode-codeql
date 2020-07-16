56b62ff758543ca52aadb6ff7841cd6e126b584c
Jason Reed <jcreedcmu@github.com>
2020-07-16 08:39:17 -0400

    Fix package deploy to not depend on rush

--- extensions/ql-vscode/gulpfile.ts/index.ts
+++ extensions/ql-vscode/gulpfile.ts/index.ts
@@ -3,7 +3,8 @@ import { compileTypeScript, watchTypeScript, copyViewCss } from './typescript';
 import { compileTextMateGrammar } from './textmate';
 import { copyTestData } from './tests';
 import { compileView } from './webpack';
+import { packageExtension } from './package';
 
 export const buildWithoutPackage = gulp.parallel(compileTypeScript, compileTextMateGrammar, compileView, copyTestData, copyViewCss);
 export { compileTextMateGrammar, watchTypeScript, compileTypeScript };
-// exports.default = gulp.series(exports.buildWithoutPackage, packageExtension);
+exports.default = gulp.series(exports.buildWithoutPackage, packageExtension);
