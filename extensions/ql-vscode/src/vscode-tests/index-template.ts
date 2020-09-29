360be5713091493776a4253aafdf048ed4dfe1a0
Andrew Eisenberg <aeisenberg@github.com>
2020-09-29 12:37:12 -0700

    Fix mocha test running
    PR #591 broke the build.

--- extensions/ql-vscode/src/vscode-tests/index-template.ts
+++ extensions/ql-vscode/src/vscode-tests/index-template.ts
@@ -5,13 +5,13 @@ import * as glob from 'glob';
 /**
  * Helper function that runs all Mocha tests found in the
  * given test root directory.
- * 
+ *
  * For each integration test suite, `vscode-test` expects
  * a test runner script exporting a function with the signature:
  * ```ts
  * export function run(): Promise<void>
  * ```
- * 
+ *
  * To create an integration test suite:
  * - create a directory beside this file
  * - create integration tests in the directory, named `<name>.test.ts`
@@ -22,16 +22,16 @@ import * as glob from 'glob';
  *   return runTestsInDirectory(__dirname);
  * }
  * ```
- * 
+ *
  * After https://github.com/microsoft/TypeScript/issues/420 is implemented,
  * this pattern can be expressed more neatly using a module interface.
  */
 export function runTestsInDirectory(testsRoot: string): Promise<void> {
   // Create the mocha test
   const mocha = new Mocha({
-    ui: 'bdd'
+    ui: 'bdd',
+    color: true
   });
-  mocha.useColors(true);
 
   return new Promise((c, e) => {
     console.log(`Adding test cases from ${testsRoot}`);
