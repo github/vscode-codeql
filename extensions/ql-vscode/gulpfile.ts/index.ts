eca534de82e32ca6dcfdc99c05a2613e1a4cd9d2
Andrew Eisenberg <aeisenberg@github.com>
2020-11-12 08:11:10 -0800

    Inject the app insights key at build time
    Uses a github secret.  When building the extension, set the
    APP_INSIGHTS_KEY environment variable to the key and the build process
    will inject this value as the key to use.

--- extensions/ql-vscode/gulpfile.ts/index.ts
+++ extensions/ql-vscode/gulpfile.ts/index.ts
@@ -4,7 +4,14 @@ import { compileTextMateGrammar } from './textmate';
 import { copyTestData } from './tests';
 import { compileView } from './webpack';
 import { packageExtension } from './package';
+import { injectAppInsightsKey } from './appInsights';
 
-export const buildWithoutPackage = gulp.parallel(compileTypeScript, compileTextMateGrammar, compileView, copyTestData, copyViewCss);
-export { compileTextMateGrammar, watchTypeScript, compileTypeScript };
-exports.default = gulp.series(exports.buildWithoutPackage, packageExtension);
+export const buildWithoutPackage =
+  gulp.series(
+    gulp.parallel(
+      compileTypeScript, compileTextMateGrammar, compileView, copyTestData, copyViewCss
+    ),
+    injectAppInsightsKey
+  );
+export { compileTextMateGrammar, watchTypeScript, compileTypeScript, injectAppInsightsKey };
+exports.default = gulp.series(buildWithoutPackage, packageExtension);
