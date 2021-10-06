cfdb5c73a743f0dbe713a2ffd4506961e96f32a3
marcnjaramillo <mnj.webdeveloper@gmail.com>
2021-10-05 17:09:39 -0700

    Create rough solution to replace non-printable characters

--- extensions/ql-vscode/src/view/RawTableValue.tsx
+++ extensions/ql-vscode/src/view/RawTableValue.tsx
@@ -10,12 +10,23 @@ interface Props {
 
 export default function RawTableValue(props: Props): JSX.Element {
   const v = props.value;
+  const codes: { [key: string]: any } = { '\n': 'U+000A', '\b': 'U+2408', '\0': 'U+0000' };
   if (
     typeof v === 'string'
     || typeof v === 'number'
     || typeof v === 'boolean'
   ) {
-    return <span>{v.toString()}</span>;
+    const text = v.toString();
+    const newV = text.split('');
+    for (let i = 0; i < newV.length; i++) {
+      for (const code in codes) {
+        if (code === newV[i]) {
+          newV[i] = codes[code];
+        }
+      }
+    }
+    const filtered = newV.join('');
+    return <span>{filtered}</span>;
   }
 
   return renderLocation(v.url, v.label, props.databaseUri);
