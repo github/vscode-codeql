3210039bab11316e9a3930b090b1c35343c8fbc6
marcnjaramillo <mnj.webdeveloper@gmail.com>
2021-10-08 12:56:56 -0700

    Finish creating check for non-printing characters

--- extensions/ql-vscode/src/view/RawTableValue.tsx
+++ extensions/ql-vscode/src/view/RawTableValue.tsx
@@ -8,38 +8,14 @@ interface Props {
   databaseUri: string;
 }
 
-const codes: { [key: string]: any } = {
-  '\0': 'U+0000',
-  '\b': 'U+0008',
-  '\t': 'U+0009',
-  '\v': 'U+000B',
-  '\r': 'U+000D'
-};
-
-function onlyNewLine(element: string, index: number, array: string[]) {
-  return element === '\n';
-}
-
 export default function RawTableValue(props: Props): JSX.Element {
   const rawValue = props.value;
-
   if (
     typeof rawValue === 'string'
     || typeof rawValue === 'number'
     || typeof rawValue === 'boolean'
   ) {
-    const text = rawValue.toString();
-    const newVal = text.split('').filter((element: string) => element !== ' ');
-    if (newVal.every(onlyNewLine)) {
-      const cleanVal = '[' + newVal.join('') + ']';
-      return <span>{cleanVal}</span>;
-    } else {
-      for (let i = 0; i < newVal.length; i++) {
-        newVal[i] = codes[newVal[i]] || newVal[i];
-      }
-      const cleanVal = newVal.join('');
-      return <span>{cleanVal}</span>;
-    }
+    return <span>{renderLocation(undefined, rawValue.toString())}</span>;
   }
 
   return renderLocation(rawValue.url, rawValue.label, props.databaseUri);
