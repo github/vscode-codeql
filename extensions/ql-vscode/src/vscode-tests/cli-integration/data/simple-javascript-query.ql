16177f8e69c5ebab50f3987daa545bbc8eec5532
shati-patel <42641846+shati-patel@users.noreply.github.com>
2021-12-15 15:33:36 +0000

    Add test for `tryGetQueryMetadata`

--- extensions/ql-vscode/src/vscode-tests/cli-integration/data/simple-javascript-query.ql
+++ extensions/ql-vscode/src/vscode-tests/cli-integration/data/simple-javascript-query.ql
@@ -1,3 +1,10 @@
+/**
+ * @name This is the name
+ * @kind problem
+ * @problem.severity warning
+ * @id javascript/example/test-query
+ */
+
 import javascript
 
 select 1
---
+++ extensions/ql-vscode/src/vscode-tests/cli-integration/helpers.test.ts
@@ -0,0 +1,39 @@
+import * as path from 'path';
+import { extensions } from 'vscode';
+import 'mocha';
+
+import { CodeQLCliServer } from '../../cli';
+import { CodeQLExtensionInterface } from '../../extension';
+import { tryGetQueryMetadata } from '../../helpers';
+import { expect } from 'chai';
+
+describe('helpers (with CLI)', function() {
+  const baseDir = path.join(__dirname, '../../../src/vscode-tests/cli-integration');
+
+  // up to 3 minutes per test
+  this.timeout(3 * 60 * 1000);
+
+  let cli: CodeQLCliServer;
+
+  beforeEach(async () => {
+    const extension = await extensions.getExtension<CodeQLExtensionInterface | Record<string, never>>('GitHub.vscode-codeql')!.activate();
+    if ('cliServer' in extension) {
+      cli = extension.cliServer;
+    } else {
+      throw new Error('Extension not initialized. Make sure cli is downloaded and installed properly.');
+    }
+  });
+
+  it('should get query metadata if it exists', async () => {
+    // Query with empty metadata
+    const noMetadata = await tryGetQueryMetadata(cli, path.join(baseDir, 'data', 'simple-query.ql'));
+    // Query with metadata
+    const metadata = await tryGetQueryMetadata(cli, path.join(baseDir, 'data', 'simple-javascript-query.ql'));
+
+    expect(noMetadata).to.deep.equal({});
+
+    expect(metadata!.name).to.equal('This is the name');
+    expect(metadata!.kind).to.equal('problem');
+    expect(metadata!.id).to.equal('javascript/example/test-query');
+  });
+});
