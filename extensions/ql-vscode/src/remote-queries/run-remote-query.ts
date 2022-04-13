37de2e7f52998e75baa630e9d1879a808eb5f74a
Andrew Eisenberg <aeisenberg@github.com>
2022-04-13 16:29:20 -0700

    Add better error messages for partial failing variant analysis
    Two scenarios handled:
    
    1. no database for existing repo
    2. repo does not exits (or no access rights for current user)
    
    In either case, an error message is sent to the logs, with a notificaiton
    in a popup.

--- extensions/ql-vscode/src/remote-queries/run-remote-query.ts
+++ extensions/ql-vscode/src/remote-queries/run-remote-query.ts
@@ -33,7 +33,12 @@ export interface QlPack {
 }
 
 interface QueriesResponse {
-  workflow_run_id: number
+  workflow_run_id: number,
+  errors?: {
+    invalid_repositories?: string[],
+    repositories_without_database?: string[],
+  },
+  repositories_queried?: string[],
 }
 
 /**
@@ -324,12 +329,33 @@ async function runRemoteQueriesApiRequest(
         data
       }
     );
-    const workflowRunId = response.data.workflow_run_id;
-    void showAndLogInformationMessage(`Successfully scheduled runs. [Click here to see the progress](https://github.com/${owner}/${repo}/actions/runs/${workflowRunId}).`);
-    return workflowRunId;
+    const { popupMessage, logMessage } = parseResponse(owner, repo, response.data);
+    void showAndLogInformationMessage(popupMessage, { fullMessage: logMessage });
+    return response.data.workflow_run_id;
   } catch (error) {
     void showAndLogErrorMessage(getErrorMessage(error));
   }
+}
+
+function parseResponse(owner: string, repo: string, response: QueriesResponse) {
+  const popupMessage = `Successfully scheduled runs. [Click here to see the progress](https://github.com/${owner}/${repo}/actions/runs/${response.workflowRunId}).`
+    + (response.errors ? '\n\nSome repositories could not be scheduled. See extension log for details.' : '');
+
+  let logMessage = `Successfully scheduled runs. See https://github.com/${owner}/${repo}/actions/runs/${response.workflow_run_id}.`;
+  if (response.errors) {
+    logMessage += '\nSome repositories could not be scheduled.';
+    if (response.errors.invalid_repositories?.length) {
+      logMessage += `\nInvalid repositories: ${response.errors.invalid_repositories.join(', ')}`;
+    }
+    if (response.errors.repositories_without_database?.length) {
+      logMessage += `\nRepositories without databases: ${response.errors.repositories_without_database.join(', ')}`;
+    }
+  }
+
+  return {
+    popupMessage,
+    logMessage
+  };
 }
 
 /**
