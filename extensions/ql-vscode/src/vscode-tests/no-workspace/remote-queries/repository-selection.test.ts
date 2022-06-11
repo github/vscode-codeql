c86d2a4cf320ffd9608c5804899bd216207b9e27
Charis Kyriakou <charisk@github.com>
2022-06-11 11:24:41 +0200

    More renames

--- extensions/ql-vscode/src/vscode-tests/no-workspace/remote-queries/repository-selection.test.ts
+++ extensions/ql-vscode/src/vscode-tests/no-workspace/remote-queries/repository-selection.test.ts
@@ -82,14 +82,14 @@ describe('repository-selection', function() {
       'owner-with-hyphens',
       'ownerWithNumbers58'
     ];
-    goodOwners.forEach(org => {
-      it(`should run on a valid org that you enter in the text box: ${org}`, async () => {
+    goodOwners.forEach(owner => {
+      it(`should run on a valid owner that you enter in the text box: ${owner}`, async () => {
         // Fake return values
         quickPickSpy.resolves(
           { useAllReposOfOwner: true }
         );
         getRemoteRepositoryListsSpy.returns({}); // no pre-defined repo lists
-        showInputBoxSpy.resolves(org);
+        showInputBoxSpy.resolves(owner);
 
         // Make the function call
         const repoSelection = await mod.getRepositorySelection();
@@ -97,7 +97,7 @@ describe('repository-selection', function() {
         // Check that the return value is correct
         expect(repoSelection.repositories).to.be.undefined;
         expect(repoSelection.repositoryLists).to.be.undefined;
-        expect(repoSelection.owners).to.deep.eq([org]);
+        expect(repoSelection.owners).to.deep.eq([owner]);
       });
     });
 
@@ -106,14 +106,14 @@ describe('repository-selection', function() {
       'invalid_owner',
       'owner-with-repo/repo'
     ];
-    badOwners.forEach(org => {
-      it(`should show an error message if you enter an invalid org in the text box: ${org}`, async () => {
+    badOwners.forEach(owner => {
+      it(`should show an error message if you enter an invalid owner in the text box: ${owner}`, async () => {
         // Fake return values
         quickPickSpy.resolves(
           { useAllReposOfOwner: true }
         );
         getRemoteRepositoryListsSpy.returns({}); // no pre-defined repo lists
-        showInputBoxSpy.resolves(org);
+        showInputBoxSpy.resolves(owner);
 
         // Function call should throw a UserCancellationException
         await expect(mod.getRepositorySelection()).to.be.rejectedWith(UserCancellationException, 'Invalid user or organization format. Please enter a valid user or organization (e.g. github)');
