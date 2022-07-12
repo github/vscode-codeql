6b1b103694b459963a340fe2d3dec2ec1b5321e2
shati-patel <42641846+shati-patel@users.noreply.github.com>
2022-07-12 12:52:08 +0100

    Replace primer `TextInput` with VS Code UI toolkit

--- extensions/ql-vscode/src/remote-queries/view/RepositoriesSearch.tsx
+++ extensions/ql-vscode/src/remote-queries/view/RepositoriesSearch.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import { ChangeEvent } from 'react';
-import { TextInput } from '@primer/react';
-import { SearchIcon } from '@primer/octicons-react';
+import { VSCodeTextField } from '@vscode/webview-ui-toolkit/react';
 
 interface RepositoriesSearchProps {
   filterValue: string;
@@ -10,20 +9,16 @@ interface RepositoriesSearchProps {
 
 const RepositoriesSearch = ({ filterValue, setFilterValue }: RepositoriesSearchProps) => {
   return <>
-    <TextInput
-      block
-      sx={{
-        backgroundColor: 'var(--vscode-editor-background);',
-        color: 'var(--vscode-editor-foreground);',
-        width: 'calc(100% - 14px)',
-      }}
-      leadingVisual={SearchIcon}
-      aria-label="Repository search"
+    <VSCodeTextField
+      placeholder='Filter by repository owner/name'
+      size='150'
+      ariaLabel="Repository search"
       name="repository-search"
-      placeholder="Filter by repository owner/name"
       value={filterValue}
-      onChange={(e: ChangeEvent) => setFilterValue((e.target as HTMLInputElement).value)}
-    />
+      onInput={(e: ChangeEvent) => setFilterValue((e.target as HTMLInputElement).value)}
+    >
+      <span slot="start" className="codicon codicon-search"></span>
+    </VSCodeTextField>
   </>;
 };
 
