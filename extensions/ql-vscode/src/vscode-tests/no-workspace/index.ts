c4df9dbec889225e00cfacd7d51d80a1c0e7833d
Elena Tanasoiu <elenatanasoiu@github.com>
2022-07-18 19:08:56 +0100

    Extract method for creating Extension context
    We'd like to re-use this to test the `exportResultsToGist` method in
    `export-results.ts`.
    
    So let's move it to a shared folder in the `vscode-tests/no-workspace` folder.
    
    Since there's no `helper.ts` file in this folder and to avoid any confusion with
    the `helpers.test.ts` file, I've opted to put this shared method into `index.ts`.
    
    Happy to be told there's a better pattern for this as it doesn't feel very nice!

--- extensions/ql-vscode/src/vscode-tests/no-workspace/index.ts
+++ extensions/ql-vscode/src/vscode-tests/no-workspace/index.ts
@@ -4,6 +4,7 @@ import * as sinonChai from 'sinon-chai';
 import * as chai from 'chai';
 import * as chaiAsPromised from 'chai-as-promised';
 import 'chai/register-should';
+import { ExtensionContext } from 'vscode';
 
 import { runTestsInDirectory } from '../index-template';
 
@@ -12,4 +13,20 @@ chai.use(sinonChai);
 
 export function run(): Promise<void> {
   return runTestsInDirectory(__dirname);
+}
+
+export function createMockExtensionContext(): ExtensionContext {
+  return {
+    globalState: {
+      _state: {
+        'telemetry-request-viewed': true
+      } as Record<string, any>,
+      get(key: string) {
+        return this._state[key];
+      },
+      update(key: string, val: any) {
+        this._state[key] = val;
+      }
+    }
+  } as any;
 }
