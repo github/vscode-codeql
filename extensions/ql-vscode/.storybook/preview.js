d15e3885d7478ec5e8a1056c493d538ab7abfe15
Koen Vlaswinkel <koesie10@github.com>
2022-09-07 16:36:24 +0200

    Add documentation about parameters in preview.js

--- extensions/ql-vscode/.storybook/preview.js
+++ extensions/ql-vscode/.storybook/preview.js
@@ -6,14 +6,18 @@ import '@vscode/codicons/dist/codicon.css';
 
 import '../src/stories/vscode-theme.css';
 
+// https://storybook.js.org/docs/react/configure/overview#configure-story-rendering
 export const parameters = {
+  // All props starting with `on` will automatically receive an action as a prop
   actions: { argTypesRegex: "^on[A-Z].*" },
+  // All props matching these names will automatically get the correct control
   controls: {
     matchers: {
       color: /(background|color)$/i,
       date: /Date$/,
     },
   },
+  // Use a dark theme to be aligned with VSCode
   docs: {
     theme: themes.dark,
   },
