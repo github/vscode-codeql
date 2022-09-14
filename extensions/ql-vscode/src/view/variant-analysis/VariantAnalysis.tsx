182c2f3b8e84dccf27c2b410048a5d4a4224b232
Koen Vlaswinkel <koesie10@github.com>
2022-09-14 14:14:13 +0200

    Add variant analysis header to view

--- extensions/ql-vscode/src/view/variant-analysis/VariantAnalysis.tsx
+++ extensions/ql-vscode/src/view/variant-analysis/VariantAnalysis.tsx
@@ -1,5 +1,21 @@
 import * as React from 'react';
+import { VariantAnalysisStatus } from '../../remote-queries/shared/variant-analysis';
+import { VariantAnalysisContainer } from './VariantAnalysisContainer';
+import { VariantAnalysisHeader } from './VariantAnalysisHeader';
 
 export function VariantAnalysis(): JSX.Element {
-  return <span>Hello!</span>;
+  return (
+    <VariantAnalysisContainer>
+      <VariantAnalysisHeader
+        queryName="Example query"
+        queryFileName="example.ql"
+        status={VariantAnalysisStatus.InProgress}
+        onOpenQueryClick={() => void 0}
+        onViewQueryClick={() => void 0}
+        onStopQueryClick={() => void 0}
+        onCopyRepositoryListClick={() => void 0}
+        onExportResultsClick={() => void 0}
+      />
+    </VariantAnalysisContainer>
+  );
 }
