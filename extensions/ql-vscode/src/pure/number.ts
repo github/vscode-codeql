cbc2650f304898fc0c15979770f388a3ce159514
Koen Vlaswinkel <koesie10@github.com>
2022-09-16 13:17:35 +0200

    Add tests for date and number formatting functions

--- extensions/ql-vscode/src/pure/number.ts
+++ extensions/ql-vscode/src/pure/number.ts
@@ -2,7 +2,7 @@
  * Contains an assortment of helper constants and functions for working with numbers.
  */
 
-const numberFormatter = new Intl.NumberFormat('en');
+const numberFormatter = new Intl.NumberFormat('en-US');
 
 export function formatDecimal(value: number): string {
   return numberFormatter.format(value);
