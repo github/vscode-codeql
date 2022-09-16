cd2b5a8c59c66d1cf2dbda9791627fb1df8e2bae
Koen Vlaswinkel <koesie10@github.com>
2022-09-16 14:36:11 +0200

    Set timezone and locale for tests

--- extensions/ql-vscode/test/pure-tests/date.test.ts
+++ extensions/ql-vscode/test/pure-tests/date.test.ts
@@ -5,7 +5,7 @@ import { formatDate } from '../../src/pure/date';
 
 describe('Date', () => {
   it('should return a formatted date', () => {
-    expect(formatDate(new Date(1663326904000))).to.eq('Sep 16, 1:15 PM');
-    expect(formatDate(new Date(1631783704000))).to.eq('Sep 16, 2021, 11:15 AM');
+    expect(formatDate(new Date(1663326904000))).to.eq('Sep 16, 11:15 AM');
+    expect(formatDate(new Date(1631783704000))).to.eq('Sep 16, 2021, 9:15 AM');
   });
 });
