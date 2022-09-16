55b060af97581d03406e0283d8bf7f578e8ade85
Koen Vlaswinkel <koesie10@github.com>
2022-09-16 13:13:10 +0200

    Use browser date format

--- extensions/ql-vscode/src/pure/date.ts
+++ extensions/ql-vscode/src/pure/date.ts
@@ -2,14 +2,14 @@
  * Contains an assortment of helper constants and functions for working with dates.
  */
 
-const dateWithoutYearFormatter = new Intl.DateTimeFormat('en', {
+const dateWithoutYearFormatter = new Intl.DateTimeFormat(undefined, {
   month: 'short',
   day: 'numeric',
   hour: 'numeric',
   minute: '2-digit',
 });
 
-const dateFormatter = new Intl.DateTimeFormat('en', {
+const dateFormatter = new Intl.DateTimeFormat(undefined, {
   year: 'numeric',
   month: 'short',
   day: 'numeric',
