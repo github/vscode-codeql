8da1a2847875fd8eed79f7793df60c10739230b4
Koen Vlaswinkel <koesie10@github.com>
2022-09-21 12:16:37 +0200

    Add documentation to formatDecimal function

--- extensions/ql-vscode/src/pure/number.ts
+++ extensions/ql-vscode/src/pure/number.ts
@@ -4,6 +4,12 @@
 
 const numberFormatter = new Intl.NumberFormat('en-US');
 
+/**
+ * Formats a number to be human-readable with decimal places and thousands separators.
+ *
+ * @param value The number to format.
+ * @returns The formatted number. For example, "10,000", "1,000,000", or "1,000,000,000".
+ */
 export function formatDecimal(value: number): string {
   return numberFormatter.format(value);
 }
