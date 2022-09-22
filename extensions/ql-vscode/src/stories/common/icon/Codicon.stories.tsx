3aced3c4d372ca67984b029fa575aa7d13325540
Koen Vlaswinkel <koesie10@github.com>
2022-09-22 17:37:29 +0200

    Add Storybook stories for icons
    This will add Storybook stories for the error, success, and warning
    icons, as well as for the generic `Codicon` component.
    
    To show the available icons for the `Codicon` component, a static JSON
    list is generated from the contents of a CSV file included as part of
    the `@vscode/codicons` npm package. The command to regenerate the file
    is included in the story.

---
+++ extensions/ql-vscode/src/stories/common/icon/Codicon.stories.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import { CodePaths, Codicon as CodiconComponent } from '../../../view/common';
+
+// To regenerate the icons, use the following command from the `extensions/ql-vscode` directory:
+// jq -R '[inputs | [splits(", *")] as $row | $row[0]]' < node_modules/@vscode/codicons/dist/codicon.csv > src/stories/common/icon/vscode-icons.json
+import icons from './vscode-icons.json';
+
+export default {
+  title: 'Icon/Codicon',
+  component: CodiconComponent,
+  argTypes: {
+    name: {
+      control: 'select',
+      options: icons,
+    },
+  },
+} as ComponentMeta<typeof CodePaths>;
+
+const Template: ComponentStory<typeof CodiconComponent> = (args) => (
+  <CodiconComponent {...args} />
+);
+
+export const Codicon = Template.bind({});
+Codicon.args = {
+  name: 'account',
+  label: 'Account'
+};
+
---
+++ extensions/ql-vscode/src/stories/common/icon/WarningIcon.stories.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import { CodePaths, WarningIcon as WarningIconComponent } from '../../../view/common';
+
+export default {
+  title: 'Icon/Warning Icon',
+  component: WarningIconComponent,
+} as ComponentMeta<typeof CodePaths>;
+
+const Template: ComponentStory<typeof WarningIconComponent> = (args) => (
+  <WarningIconComponent {...args} />
+);
+
+export const WarningIcon = Template.bind({});
