c25793aa6d85afe2bdd9e5317d006c6d71060658
Elena Tanasoiu <elenatanasoiu@github.com>
2022-09-30 11:48:06 +0100

    WIP

--- extensions/ql-vscode/src/remote-queries/variant-analysis-manager.ts
+++ extensions/ql-vscode/src/remote-queries/variant-analysis-manager.ts
@@ -1,29 +1,29 @@
-import { CancellationToken, commands, ExtensionContext } from 'vscode';
-import { DisposableObject } from '../pure/disposable-object';
-import { Logger } from '../logging';
-import { VariantAnalysisMonitor } from './variant-analysis-monitor';
+// import { CancellationToken, commands, ExtensionContext } from 'vscode';
+// import { DisposableObject } from '../pure/disposable-object';
+// import { Logger } from '../logging';
+// import { VariantAnalysisMonitor } from './variant-analysis-monitor';
 
-export class RemoteQueriesManager extends DisposableObject {
-  private readonly variantAnalysisMonitor: VariantAnalysisMonitor;
+// export class RemoteQueriesManager extends DisposableObject {
+//   private readonly variantAnalysisMonitor: VariantAnalysisMonitor;
 
-  constructor(
-    private readonly ctx: ExtensionContext,
-    private readonly storagePath: string,
-    logger: Logger,
-  ) {
-    super();
-    this.variantAnalysisMonitor = new VariantAnalysisMonitor(ctx, logger);
+//   constructor(
+//     private readonly ctx: ExtensionContext,
+//     private readonly storagePath: string,
+//     logger: Logger,
+//   ) {
+//     super();
+//     this.variantAnalysisMonitor = new VariantAnalysisMonitor(ctx, logger);
 
-    this.push(this.view);
-  }
+//     this.push(this.view);
+//   }
 
-  public async monitorRemoteQuery(
-    queryId: string,
-    variant: RemoteQuery,
-    cancellationToken: CancellationToken
-  ): Promise<void> {
-    const credentials = await Credentials.initialize(this.ctx);
+//   public async monitorRemoteQuery(
+//     queryId: string,
+//     variant: RemoteQuery,
+//     cancellationToken: CancellationToken
+//   ): Promise<void> {
+//     const credentials = await Credentials.initialize(this.ctx);
 
-    const queryWorkflowResult = await this.remoteQueriesMonitor.monitorQuery(remoteQuery, cancellationToken);
-  }
-}
+//     const queryWorkflowResult = await this.remoteQueriesMonitor.monitorQuery(remoteQuery, cancellationToken);
+//   }
+// }
