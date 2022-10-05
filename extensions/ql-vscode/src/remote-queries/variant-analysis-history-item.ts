64a0f549c99d9e2d87c1efaa2d31b3b51b8da597
shati-patel <42641846+shati-patel@users.noreply.github.com>
2022-10-05 20:58:30 +0100

    Add new `VariantAnalysisHistoryItem` type

---
+++ extensions/ql-vscode/src/remote-queries/variant-analysis-history-item.ts
@@ -0,0 +1,15 @@
+import { QueryStatus } from '../query-status';
+import { VariantAnalysis } from './shared/variant-analysis';
+
+/**
+ * Information about a variant analysis.
+ */
+export interface VariantAnalysisHistoryItem {
+  readonly t: 'variant-analysis';
+  failureReason?: string;
+  resultCount?: number;
+  status: QueryStatus;
+  completed: boolean;
+  variantAnalysis: VariantAnalysis;
+  userSpecifiedLabel?: string;
+}
