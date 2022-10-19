d16156a3a3b27da0cb46fc36960571a28ae10bb9
Charis Kyriakou <charisk@github.com>
2022-10-19 11:46:18 +0100

    Initial setup around GitHub API mock server

---
+++ extensions/ql-vscode/src/mocks/mock-gh-api-server.ts
@@ -0,0 +1,19 @@
+export function startServer(): void {
+  // TODO: Enable HTTP interception.
+}
+
+export function stopServer(): void {
+  // Disable HTTP interception.
+}
+
+export function loadScenario(): void {
+  // TODO: Implement logic to load a scenario from a directory.
+}
+
+export function listScenarios(): void {
+  // TODO: Implement logic to list all available scenarios.
+}
+
+export function recordScenario(): void {
+  // TODO: Implement logic to record a new scenario to a directory.
+}
