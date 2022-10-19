bb0595d5695c5db0b7c3f79e1a2b1040e8fbf700
Charis Kyriakou <charisk@github.com>
2022-10-19 12:23:00 +0100

    Convert GitHub API client to a class

--- extensions/ql-vscode/src/mocks/mock-gh-api-server.ts
+++ extensions/ql-vscode/src/mocks/mock-gh-api-server.ts
@@ -1,19 +1,24 @@
-export function startServer(): void {
-  // TODO: Enable HTTP interception.
-}
+/**
+ * Enables mocking of the GitHub API server via HTTP interception, using msw.
+ */
+export class MockGitHubApiServer {
+  public startServer(): void {
+    // TODO: Enable HTTP interception.
+  }
 
-export function stopServer(): void {
-  // Disable HTTP interception.
-}
+  public stopServer(): void {
+    // Disable HTTP interception.
+  }
 
-export function loadScenario(): void {
-  // TODO: Implement logic to load a scenario from a directory.
-}
+  public loadScenario(): void {
+    // TODO: Implement logic to load a scenario from a directory.
+  }
 
-export function listScenarios(): void {
-  // TODO: Implement logic to list all available scenarios.
-}
+  public listScenarios(): void {
+    // TODO: Implement logic to list all available scenarios.
+  }
 
-export function recordScenario(): void {
-  // TODO: Implement logic to record a new scenario to a directory.
+  public recordScenario(): void {
+    // TODO: Implement logic to record a new scenario to a directory.
+  }
 }
