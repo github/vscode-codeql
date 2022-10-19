284896da6c2569a16cce29d3f3d5c197d5dc357b
Charis Kyriakou <charisk@github.com>
2022-10-19 14:30:36 +0100

    Add mock GitHub API config listener

--- extensions/ql-vscode/src/mocks/mock-gh-api-server.ts
+++ extensions/ql-vscode/src/mocks/mock-gh-api-server.ts
@@ -1,13 +1,28 @@
+import { MockGitHubApiConfigListener } from '../config';
+
 /**
  * Enables mocking of the GitHub API server via HTTP interception, using msw.
  */
 export class MockGitHubApiServer {
+  private isListening: boolean;
+  private config: MockGitHubApiConfigListener;
+
+  constructor() {
+    this.isListening = false;
+    this.config = new MockGitHubApiConfigListener();
+    this.setupConfigListener();
+  }
+
   public startServer(): void {
+    this.isListening = true;
+
     // TODO: Enable HTTP interception.
   }
 
   public stopServer(): void {
-    // Disable HTTP interception.
+    this.isListening = false;
+
+    // TODO: Disable HTTP interception.
   }
 
   public loadScenario(): void {
@@ -20,5 +35,15 @@ export class MockGitHubApiServer {
 
   public recordScenario(): void {
     // TODO: Implement logic to record a new scenario to a directory.
+  }
+
+  private setupConfigListener(): void {
+    this.config.onDidChangeConfiguration(() => {
+      if (this.config.mockServerEnabled && !this.isListening) {
+        this.startServer();
+      } else if (!this.config.mockServerEnabled && this.isListening) {
+        this.stopServer();
+      }
+    });
   }
 }
