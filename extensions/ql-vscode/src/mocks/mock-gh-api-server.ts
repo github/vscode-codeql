424520613e4d20526255e71514debb57fe553ede
Charis Kyriakou <charisk@users.noreply.github.com>
2022-10-19 13:05:28 +0100

    Initial setup around GitHub API mock server (#1629)

---
+++ extensions/ql-vscode/src/mocks/mock-gh-api-server.ts
@@ -0,0 +1,24 @@
+/**
+ * Enables mocking of the GitHub API server via HTTP interception, using msw.
+ */
+export class MockGitHubApiServer {
+  public startServer(): void {
+    // TODO: Enable HTTP interception.
+  }
+
+  public stopServer(): void {
+    // Disable HTTP interception.
+  }
+
+  public loadScenario(): void {
+    // TODO: Implement logic to load a scenario from a directory.
+  }
+
+  public listScenarios(): void {
+    // TODO: Implement logic to list all available scenarios.
+  }
+
+  public recordScenario(): void {
+    // TODO: Implement logic to record a new scenario to a directory.
+  }
+}
