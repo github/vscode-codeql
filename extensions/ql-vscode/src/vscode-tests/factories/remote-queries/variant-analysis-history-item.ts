d610701d458d12b0d2155f1c948ca711e2823ea0
Elena Tanasoiu <elenatanasoiu@github.com>
2022-11-02 09:44:25 +0000

    Use factory method to generate variant analysis

--- extensions/ql-vscode/src/vscode-tests/factories/remote-queries/variant-analysis-history-item.ts
+++ extensions/ql-vscode/src/vscode-tests/factories/remote-queries/variant-analysis-history-item.ts
@@ -1,7 +1,7 @@
-import { faker } from '@faker-js/faker';
 import { VariantAnalysisHistoryItem } from '../../../remote-queries/variant-analysis-history-item';
 import { QueryStatus } from '../../../query-status';
 import { VariantAnalysisStatus } from '../../../remote-queries/shared/variant-analysis';
+import { createMockVariantAnalysis } from './shared/variant-analysis';
 
 export function createMockVariantAnalysisHistoryItem(
   historyItemStatus: QueryStatus = QueryStatus.InProgress,
@@ -16,24 +16,7 @@ export function createMockVariantAnalysisHistoryItem(
     resultCount,
     status: historyItemStatus,
     completed: false,
-    variantAnalysis: {
-      'id': faker.datatype.number(),
-      'controllerRepoId': faker.datatype.number(),
-      'query': {
-        'name': 'Variant Analysis Query History Item',
-        'filePath': 'PLACEHOLDER/q2.ql',
-        'language': 'ruby',
-        'text': '/**\n * @name Variant Analysis Query History Item\n * @kind problem\n * @problem.severity warning\n * @id ruby/example/empty-block\n */\nimport ruby\n\nfrom Block b\nwhere b.getNumberOfStatements() = 0\nselect b, \'This is an empty block.\'\n'
-      },
-      'databases': {
-        'repositories': ['92384123', '1230871']
-      },
-      'createdAt': faker.date.recent().toISOString(),
-      'updatedAt': faker.date.recent().toISOString(),
-      'executionStartTime': faker.date.recent().toISOString(),
-      'status': variantAnalysisStatus,
-      'actionsWorkflowRunId': faker.datatype.number()
-    },
+    variantAnalysis: createMockVariantAnalysis(variantAnalysisStatus),
     userSpecifiedLabel,
   } as unknown) as VariantAnalysisHistoryItem;
 }
