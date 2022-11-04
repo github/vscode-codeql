3cc7d3ce8a8f70dceb7fd2a062b7baf830611462
Nora <norascheuch@github.com>
2022-11-04 13:06:31 +0100

    Add sample error
    Co-authored-by: Charis Kyriakou <charisk@users.noreply.github.com>

--- extensions/ql-vscode/src/databases/ui/db-tree-view-item.ts
+++ extensions/ql-vscode/src/databases/ui/db-tree-view-item.ts
@@ -2,11 +2,13 @@ import * as vscode from 'vscode';
 import { DbItem } from '../db-item';
 
 /**
- * Represents an item in the database tree view.
+ * Represents an item in the database tree view. This item could be 
+ * representing an actual database item or an error.
  */
 export class DbTreeViewItem extends vscode.TreeItem {
   constructor(
-    public readonly dbItem: DbItem,
+    public readonly dbItem: DbItem | undefined,
+    public readonly iconPath: vscode.ThemeIcon,
     public readonly label: string,
     public readonly tooltip: string,
     public readonly collapsibleState: vscode.TreeItemCollapsibleState,
@@ -14,4 +16,15 @@ export class DbTreeViewItem extends vscode.TreeItem {
   ) {
     super(label, collapsibleState);
   }
+}
+
+export function createDbTreeViewItemWarning(label: string, tooltip: string): DbTreeViewItem {
+  return new DbTreeViewItem(
+    undefined,
+    new vscode.ThemeIcon('warning', new vscode.ThemeColor('problemsWarningIcon.foreground')),
+    label,
+    tooltip,
+    vscode.TreeItemCollapsibleState.None,
+    []
+  );
 }
