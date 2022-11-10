a80ace53344c6f9ad83c82869985d883b2c37f9b
Koen Vlaswinkel <koesie10@github.com>
2022-11-10 15:07:58 +0100

    Add alert message for failed Actions workflow
    This also changes the failure reason alert component to remove the logs
    button since it's not used by any failure reason. Instead, a link is
    added into the message for a failed Actions workflow using which the
    Actions workflow run may be opened.

--- extensions/ql-vscode/src/view/variant-analysis/FailureReasonAlert.tsx
+++ extensions/ql-vscode/src/view/variant-analysis/FailureReasonAlert.tsx
@@ -1,30 +1,12 @@
 import * as React from 'react';
-import { VSCodeButton } from '@vscode/webview-ui-toolkit/react';
+import { ReactNode } from 'react';
+import { VSCodeLink } from '@vscode/webview-ui-toolkit/react';
 import { Alert } from '../common';
 import { vscode } from '../vscode-api';
 import { VariantAnalysisFailureReason } from '../../remote-queries/shared/variant-analysis';
 
 type Props = {
   failureReason: VariantAnalysisFailureReason;
-  showLogsButton: boolean;
-};
-
-const getTitle = (failureReason: VariantAnalysisFailureReason): string => {
-  switch (failureReason) {
-    case VariantAnalysisFailureReason.NoReposQueried:
-      return 'No repositories to analyze';
-    case VariantAnalysisFailureReason.InternalError:
-      return 'Something unexpected happened';
-  }
-};
-
-const getMessage = (failureReason: VariantAnalysisFailureReason): string => {
-  switch (failureReason) {
-    case VariantAnalysisFailureReason.NoReposQueried:
-      return 'No repositories available after processing. No repositories were analyzed.';
-    case VariantAnalysisFailureReason.InternalError:
-      return 'An internal error occurred while running this variant analysis. Please try again later.';
-  }
 };
 
 const openLogs = () => {
@@ -33,16 +15,36 @@ const openLogs = () => {
   });
 };
 
+const getTitle = (failureReason: VariantAnalysisFailureReason): string => {
+  switch (failureReason) {
+    case VariantAnalysisFailureReason.NoReposQueried:
+      return 'No repositories to analyze';
+    case VariantAnalysisFailureReason.ActionsWorkflowRunFailed:
+      return 'GitHub Actions workflow run failed';
+    case VariantAnalysisFailureReason.InternalError:
+      return 'Something unexpected happened';
+  }
+};
+
+const getMessage = (failureReason: VariantAnalysisFailureReason): ReactNode => {
+  switch (failureReason) {
+    case VariantAnalysisFailureReason.NoReposQueried:
+      return 'No repositories available after processing. No repositories were analyzed.';
+    case VariantAnalysisFailureReason.ActionsWorkflowRunFailed:
+      return <>The GitHub Actions workflow run has failed. <VSCodeLink onClick={openLogs}>Check logs</VSCodeLink> and try running this query again.</>;
+    case VariantAnalysisFailureReason.InternalError:
+      return 'An internal error occurred while running this variant analysis. Please try again later.';
+  }
+};
+
 export const FailureReasonAlert = ({
   failureReason,
-  showLogsButton,
 }: Props) => {
   return (
     <Alert
       type="error"
       title={getTitle(failureReason)}
       message={getMessage(failureReason)}
-      actions={showLogsButton && <VSCodeButton appearance="secondary" onClick={openLogs}>View logs</VSCodeButton>}
     />
   );
 };
