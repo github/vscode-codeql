90dd97fbc021fcb0fac3369d30bf7a697d21442f
Koen Vlaswinkel <koesie10@github.com>
2022-11-23 10:26:39 +0100

    Use correct jest-runner-vscode for no-workspace

--- extensions/ql-vscode/src/vscode-tests/no-workspace/jest-runner-vscode.config.ts
+++ extensions/ql-vscode/src/vscode-tests/no-workspace/jest-runner-vscode.config.ts
@@ -1,20 +1,9 @@
-import * as path from "path";
-import * as tmp from "tmp-promise";
 import { RunnerOptions } from "jest-runner-vscode";
 
-const tmpDir = tmp.dirSync({ unsafeCleanup: true });
+import baseConfig from "../jest-runner-vscode.config.base";
 
 const config: RunnerOptions = {
-  version: "stable",
-  launchArgs: [
-    "--disable-extensions",
-    "--disable-gpu",
-    "--new-window",
-    "--user-data-dir=" + path.join(tmpDir.name, "user-data"),
-  ],
-  workspaceDir: path.resolve(__dirname, "test/data"),
-  openInFolder: true,
-  extensionDevelopmentPath: path.resolve(__dirname),
+  ...baseConfig,
 };
 
 // We are purposefully not using export default here since that would result in an ESModule, which doesn't seem to be
