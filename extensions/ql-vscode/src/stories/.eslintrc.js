195ef4560029bdf6ab95211a52a5c352196c519b
Elena Tanasoiu <elenatanasoiu@github.com>
2022-11-24 19:37:53 +0000

    Enable eslint-plugin-github for storybook
    This also adds the recommended rules for React.

--- extensions/ql-vscode/src/stories/.eslintrc.js
+++ extensions/ql-vscode/src/stories/.eslintrc.js
@@ -2,11 +2,23 @@ module.exports = {
   env: {
     browser: true
   },
+  plugins: [
+    "github",
+  ],
   extends: [
+    "plugin:github/react",
+    "plugin:github/recommended",
+    "plugin:github/typescript",
     "plugin:react/recommended",
     "plugin:react-hooks/recommended",
     "plugin:storybook/recommended",
   ],
+  rules: {
+    "filenames/match-regex": "off",
+    "import/named": "off",
+    "import/no-unresolved": "off",
+    "no-unused-vars": "off",
+  },
   settings: {
     react: {
       version: 'detect'
