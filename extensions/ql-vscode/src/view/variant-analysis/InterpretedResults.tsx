ce404433f7f97471ac0e84ecadd278e39a3d2d3d
Koen Vlaswinkel <koesie10@github.com>
2022-12-05 11:42:58 +0100

    Add virtualization to interpreted results
    This adds react-window and uses it to virtualize the interpreted
    results. This is a very basic implementation that tries to guess what
    the height of an item will be, but doesn't take into account the actual
    text size or line height.
    
    This also doesn't automatically adjust the size of the interpreted
    results container to the size of the enclosing container. I'm also not
    entirely sure that this can be used to virtualize an expanded list,
    unless we also set the results container to have a fixed height.

--- extensions/ql-vscode/src/view/variant-analysis/InterpretedResults.tsx
+++ extensions/ql-vscode/src/view/variant-analysis/InterpretedResults.tsx
@@ -1,9 +1,11 @@
 import * as React from "react";
 import styled from "styled-components";
+import { VariableSizeList } from "react-window";
 import { AnalysisAlert } from "../../remote-queries/shared/analysis-result";
 import AnalysisAlertResult from "../remote-queries/AnalysisAlertResult";
+import { useCallback } from "react";
 
-const Container = styled.ul`
+const Container = styled(VariableSizeList)`
   list-style-type: none;
   margin: 1em 0 0;
   padding: 0.5em 0 0 0;
@@ -21,13 +23,46 @@ export type InterpretedResultsProps = {
 export const InterpretedResults = ({
   interpretedResults,
 }: InterpretedResultsProps) => {
+  const itemSize = useCallback(
+    (index: number) => {
+      const alert = interpretedResults[index];
+
+      // TODO: Determine whether we can use this value or whether it should be calculated.
+      const baseHeight = 130;
+
+      // TODO: Take into account wrapping?
+      const codeSnippetLines = alert.codeSnippet?.text.split("\n").length ?? 0;
+      // TODO: Use actual line size
+      const codeSnippetHeight = codeSnippetLines * 16;
+
+      // TODO: Take into account wrapping?
+      const messageLines = alert.message.tokens.reduce(
+        (acc, t) => acc + t.text.split("\n").length - 1,
+        1,
+      );
+      // TODO: Use actual line size
+      const messageHeight = messageLines * 16;
+
+      return baseHeight + messageHeight + codeSnippetHeight;
+    },
+    [interpretedResults],
+  );
+
   return (
-    <Container>
-      {interpretedResults.map((r, i) => (
-        <InterpretedResultItem key={i}>
-          <AnalysisAlertResult alert={r} />
+    <Container
+      // TODO: Set this height properly. Normally, this would be based on the height of the parent element, but since
+      // we're in an expanded panel, we can't use that. Therefore, we should find some other method, the simplest of
+      // which would just have a fixed height for this panel.
+      height={500}
+      itemCount={interpretedResults.length}
+      itemSize={itemSize}
+      width="100%"
+    >
+      {({ index, style }) => (
+        <InterpretedResultItem style={style}>
+          <AnalysisAlertResult alert={interpretedResults[index]} />
         </InterpretedResultItem>
-      ))}
+      )}
     </Container>
   );
 };
