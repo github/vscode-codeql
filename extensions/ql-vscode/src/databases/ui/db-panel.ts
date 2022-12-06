7ef75f59719336f9205374bcbcd8969a9ef8ce96
Shati Patel <42641846+shati-patel@users.noreply.github.com>
2022-12-06 16:00:08 +0000

    New DB panel: Add UI command to select database item (#1840)

--- extensions/ql-vscode/src/databases/ui/db-panel.ts
+++ extensions/ql-vscode/src/databases/ui/db-panel.ts
@@ -3,6 +3,7 @@ import { commandRunner } from "../../commandRunner";
 import { DisposableObject } from "../../pure/disposable-object";
 import { DbManager } from "../db-manager";
 import { DbTreeDataProvider } from "./db-tree-data-provider";
+import { DbTreeViewItem } from "./db-tree-view-item";
 
 export class DbPanel extends DisposableObject {
   private readonly dataProvider: DbTreeDataProvider;
@@ -26,11 +27,26 @@ export class DbPanel extends DisposableObject {
         this.openConfigFile(),
       ),
     );
+    this.push(
+      commandRunner(
+        "codeQLDatabasesExperimental.setSelectedItem",
+        (treeViewItem: DbTreeViewItem) => this.setSelectedItem(treeViewItem),
+      ),
+    );
   }
 
   private async openConfigFile(): Promise<void> {
     const configPath = this.dbManager.getConfigPath();
     const document = await workspace.openTextDocument(configPath);
     await window.showTextDocument(document);
+  }
+
+  private async setSelectedItem(treeViewItem: DbTreeViewItem): Promise<void> {
+    if (treeViewItem.dbItem === undefined) {
+      throw new Error(
+        "Not a selectable database item. Please select a valid item.",
+      );
+    }
+    await this.dbManager.setSelectedDbItem(treeViewItem.dbItem);
   }
 }
