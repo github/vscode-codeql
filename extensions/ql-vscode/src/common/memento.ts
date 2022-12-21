19a9ad38d56bb513d5e3b30d47ab1c3aa96360bc
Charis Kyriakou <charisk@users.noreply.github.com>
2022-12-21 17:58:34 +0000

    Add workspace state to app container (#1902)

---
+++ extensions/ql-vscode/src/common/memento.ts
@@ -0,0 +1,44 @@
+/**
+ * A memento represents a storage utility. It can store and retrieve
+ * values.
+ *
+ * It is an interface used by the VS Code API. We replicate it here
+ * to avoid the dependency to the VS Code API.
+ */
+export interface Memento {
+  /**
+   * Returns the stored keys.
+   *
+   * @return The stored keys.
+   */
+  keys(): readonly string[];
+
+  /**
+   * Return a value.
+   *
+   * @param key A string.
+   * @return The stored value or `undefined`.
+   */
+  get<T>(key: string): T | undefined;
+
+  /**
+   * Return a value.
+   *
+   * @param key A string.
+   * @param defaultValue A value that should be returned when there is no
+   * value (`undefined`) with the given key.
+   * @return The stored value or the defaultValue.
+   */
+  get<T>(key: string, defaultValue: T): T;
+
+  /**
+   * Store a value. The value must be JSON-stringifyable.
+   *
+   * *Note* that using `undefined` as value removes the key from the underlying
+   * storage.
+   *
+   * @param key A string.
+   * @param value A value. MUST not contain cyclic references.
+   */
+  update(key: string, value: any): Thenable<void>;
+}
