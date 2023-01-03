a7be6cf31a04938782cf365646e84352c7fcf709
Charis Kyriakou <charisk@users.noreply.github.com>
2023-01-03 09:26:13 +0000

    Fix broken unit test that relies on date (#1914)

--- extensions/ql-vscode/test/unit-tests/pure/date.test.ts
+++ extensions/ql-vscode/test/unit-tests/pure/date.test.ts
@@ -2,7 +2,7 @@ import { formatDate } from "../../../src/pure/date";
 
 describe("Date", () => {
   it("should return a formatted date", () => {
-    expect(formatDate(new Date(1663326904000))).toBe("Sep 16, 11:15 AM");
+    expect(formatDate(new Date(1663326904000))).toBe("Sep 16, 2022, 11:15 AM");
     expect(formatDate(new Date(1631783704000))).toBe("Sep 16, 2021, 9:15 AM");
   });
 });
