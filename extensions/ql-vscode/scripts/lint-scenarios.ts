c99fd04aa5332345063eb30f3bacc8bf56541525
Koen Vlaswinkel <koen@vlaswinkel.info>
2023-01-10 13:23:57 +0200

    Add support for invalid files to scenario linting
    One of the scenario files doesn't match the schema because it's testing
    that the io-ts error is being thrown. This will add support for having
    invalid files in the scenarios directory.

--- extensions/ql-vscode/scripts/lint-scenarios.ts
+++ extensions/ql-vscode/scripts/lint-scenarios.ts
@@ -1,3 +1,4 @@
+import { createHash } from "crypto";
 import { pathExists, readFile } from "fs-extra";
 import { resolve, relative } from "path";
 
@@ -5,13 +6,20 @@ import { isLeft } from "fp-ts/Either";
 
 import { getFiles } from "./util/files";
 import { GitHubApiRequest } from "../src/mocks/gh-api-request";
-import { getContextPath, getMessage } from "../src/pure/io-ts";
+import { getContextPath, getErrors, getMessage } from "../src/pure/io-ts";
 
 const extensionDirectory = resolve(__dirname, "..");
 const rootDirectory = resolve(extensionDirectory, "../..");
 const scenariosDirectory = resolve(extensionDirectory, "src/mocks/scenarios");
 
 const debug = process.env.RUNNER_DEBUG || process.argv.includes("--debug");
+
+// A mapping from filename to error hash. The hash is there to ensure the
+// linting will start failing when the error message changes.
+const expectedInvalid: Record<string, string> = {
+  "invalid-github-api-response/1-submitVariantAnalysis.json":
+    "f9eed19b2c4d013014549a63cddfdc4611bdf34cc4baad5c7088e64f53c5e405",
+};
 
 async function lintScenarios() {
   let invalidFiles = 0;
@@ -32,15 +40,49 @@ async function lintScenarios() {
 
     const result = GitHubApiRequest.decode(data);
 
+    const expectedInvalidFilename = relative(scenariosDirectory, file);
+
     if (isLeft(result)) {
-      result.left.forEach((error) => {
-        // https://docs.github.com/en/actions/using-workflows/workflow-commands-for-github-actions#setting-an-error-message
-        console.log(
-          `::error file=${relative(rootDirectory, file)}::${getContextPath(
-            error.context,
-          )}: ${getMessage(error)}`,
-        );
-      });
+      // The file is invalid.
+      const hash = createHash("sha256")
+        .update(JSON.stringify(getErrors(result)))
+        .digest("hex");
+
+      if (expectedInvalidFilename in expectedInvalid) {
+        // The file is expected to be invalid
+        if (expectedInvalid[expectedInvalidFilename] !== hash) {
+          console.log(
+            `::error file=${relative(
+              rootDirectory,
+              file,
+            )}::Hash mismatch for invalid scenario: ${hash}, expected: ${
+              expectedInvalid[expectedInvalidFilename]
+            }`,
+          );
+          invalidFiles++;
+        } else if (debug) {
+          console.log(
+            `File '${relative(rootDirectory, file)}' is invalid as expected`,
+          );
+        }
+      } else {
+        result.left.forEach((error) => {
+          // https://docs.github.com/en/actions/using-workflows/workflow-commands-for-github-actions#setting-an-error-message
+          console.log(
+            `::error file=${relative(rootDirectory, file)}::${getContextPath(
+              error.context,
+            )}: ${getMessage(error)}`,
+          );
+        });
+        invalidFiles++;
+      }
+    } else if (expectedInvalidFilename in expectedInvalid) {
+      console.log(
+        `::error file=${relative(
+          rootDirectory,
+          file,
+        )}::Expected scenario to be invalid, but it is valid`,
+      );
       invalidFiles++;
     } else if (debug) {
       console.log(`File '${relative(rootDirectory, file)}' is valid`);
