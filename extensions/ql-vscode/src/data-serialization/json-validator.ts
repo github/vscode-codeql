94b383b97be82f8bb04a4c4a76a51312b4083c50
Elena Tanasoiu <elenatanasoiu@github.com>
2023-01-27 12:52:26 +0000

    Pregenerate validator functions
    In a future PR we'll store the JSON validator in the app's global
    state, so we'll be able to initialize these validator functions
    once and re-use them.

--- extensions/ql-vscode/src/data-serialization/json-validator.ts
+++ extensions/ql-vscode/src/data-serialization/json-validator.ts
@@ -1,4 +1,4 @@
-import Ajv from "ajv";
+import Ajv, { ValidateFunction } from "ajv";
 import { JsonSchemaTypes } from "./json-schema-types";
 import * as fs from "fs";
 import { JSONSchema7 as JSONSchema } from "json-schema";
@@ -7,15 +7,17 @@ export class JsonValidator {
   ajv: Ajv;
   schemaTypes: string[];
   schemas: { [key: string]: JSONSchema };
+  validators: { [key: string]: ValidateFunction };
 
-  constructor(schemaTypes?: string[]) {
+  constructor() {
     this.ajv = new Ajv();
-    this.schemaTypes = schemaTypes || JsonSchemaTypes;
-    this.schemas = this.loadSchemas(this.schemaTypes);
+    this.schemaTypes = JsonSchemaTypes;
+    this.schemas = this.loadSchemas();
+    this.validators = this.loadValidators();
   }
 
   public validate(object: unknown, typeName: string) {
-    const validate = this.createValidatorFunction(typeName);
+    const validate = this.loadValidator(typeName);
 
     const valid = validate(object);
 
@@ -34,10 +36,10 @@ export class JsonValidator {
     return this.ajv.compile(schema);
   }
 
-  private loadSchemas(schemaTypes: string[]): { [key: string]: JSONSchema } {
+  private loadSchemas(): { [key: string]: JSONSchema } {
     const schemaList: { [key: string]: JSONSchema } = {};
 
-    for (const typeName of schemaTypes) {
+    for (const typeName of this.schemaTypes) {
       const schemaFilepath = `src/data-serialization/generated-schemas/${typeName}.json`;
       schemaList[typeName] = JSON.parse(
         fs.readFileSync(schemaFilepath, "utf-8"),
@@ -48,5 +50,18 @@ export class JsonValidator {
 
   private loadSchema(typeName: string): JSONSchema {
     return this.schemas[typeName];
+  }
+
+  private loadValidators(): { [key: string]: ValidateFunction } {
+    const validatorList: { [key: string]: ValidateFunction } = {};
+
+    for (const typeName of this.schemaTypes) {
+      validatorList[typeName] = this.createValidatorFunction(typeName);
+    }
+    return validatorList;
+  }
+
+  private loadValidator(typeName: string): ValidateFunction {
+    return this.validators[typeName];
   }
 }
