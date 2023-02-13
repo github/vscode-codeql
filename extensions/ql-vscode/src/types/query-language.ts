73838ffc6cddbb3bd584df9bdca3c6ae377ade9e
Elena Tanasoiu <elenatanasoiu@github.com>
2023-02-13 19:32:11 +0000

    Introduce types folder

---
+++ extensions/ql-vscode/src/types/query-language.ts
@@ -0,0 +1,10 @@
+export enum QueryLanguage {
+  CSharp = "csharp",
+  Cpp = "cpp",
+  Go = "go",
+  Java = "java",
+  Javascript = "javascript",
+  Python = "python",
+  Ruby = "ruby",
+  Swift = "swift",
+}
