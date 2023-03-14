30ae1ba70eea60d33d5252e46f9625a3d4e204e7
Elena Tanasoiu <elenatanasoiu@github.com>
2023-03-14 21:12:55 +0000

    WIP

---
+++ extensions/ql-vscode/cypress/e2e/required/viewing-results-for-unsafe-jquery-plugin.cy.js
@@ -0,0 +1,72 @@
+/// <reference types="cypress" />
+
+describe('CodeQL Extension', () => {
+  before(() => {
+    cy.visit('http://localhost:8080')
+  })
+
+  it('Should load vscode', () => {
+    cy.get('.explorer-viewlet').should('exist')
+  })
+})
+
+
+// it('should open UnsageJQueryPlugin.ql', () => {
+//   cy.get('.explorer-viewlet')
+//     .type('{meta}{shift}P')
+
+//   cy.focused().type('Open Query')
+
+//   cy.findByText('Open Query').click()
+
+//   cy.findByText('UnsafeJQueryPlugin.ql').click()
+// });
+
+// it('Should trust project', () => {
+//   cy.get('Yes, I trust the authors')
+//   .find('div')
+//   .should(($div) => {
+//     if ($div.length !== 1) {
+//       cy.contains('Yes, I trust the authors').click()
+//     }
+//   });
+// })
+
+// it('Should execute Hello World command', () => {
+  //open the command palette
+  // cy.get('.explorer-viewlet')
+  //   .type('{meta}{shift}P')
+
+  // //type Hello World and enter
+  // cy.focused().type('Hello World')
+
+  // // assert that the notification is opened.
+  // y.findByText('Hello World!').should('exist')
+// })
+
+// 1. Open the [UnsafeJQueryPlugin query](https://github.com/github/codeql/blob/main/javascript/ql/src/Security/CWE-079/UnsafeJQueryPlugin.ql).
+// 2. Run a MRVA against the following repo list:
+
+//    ```json
+//    {
+//       "name": "test-repo-list",
+//       "repositories": [
+//          "angular-cn/ng-nice",
+//          "apache/hadoop",
+//          "apache/hive"
+//       ]
+//    }
+//    ```
+
+// 3. Check that a notification message pops up and the results view is opened.
+// 4. Check the query history. It should:
+//    - Show that an item has been added to the query history
+//    - The item should be marked as "in progress".
+// 5. Once the query starts:
+//    - Check the results view
+//    - Check the code paths view, including the code paths drop down menu.
+//    - Check that the repository filter box works
+//    - Click links to files/locations on GitHub
+//    - Check that the query history item is updated to show the number of results
+// 6. Once the query completes:
+//    - Check that the query history item is updated to show the query status as "complete"
