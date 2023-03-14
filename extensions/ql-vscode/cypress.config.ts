17a7528dbc2677cc9e1407832013caefd873e8e9
Elena Tanasoiu <elenatanasoiu@github.com>
2023-03-14 21:39:45 +0000

    Disable video and screenshots

--- extensions/ql-vscode/cypress.config.ts
+++ extensions/ql-vscode/cypress.config.ts
@@ -5,5 +5,7 @@ export default defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    screenshotOnRunFailure: false,
+    video: false,
   },
 });
