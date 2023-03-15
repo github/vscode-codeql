fce51ca9a221c187306982eda67032abc0deec61
Koen Vlaswinkel <koesie10@github.com>
2023-03-15 15:41:53 +0100

    Convert query history commands to typed commands

--- extensions/ql-vscode/src/common/commands.ts
+++ extensions/ql-vscode/src/common/commands.ts
@@ -1,5 +1,13 @@
-import { CommandManager } from "../packages/commands";
+import type { CommandManager } from "../packages/commands";
 import type { Uri } from "vscode";
+import type { QueryHistoryInfo } from "../query-history/query-history-info";
+
+// A command function matching the signature that VS Code calls when
+// a command on a selection is invoked.
+export type SelectionCommandFunction<Item> = (
+  singleItem: Item,
+  multiSelect: Item[],
+) => Promise<void>;
 
 /**
  * Contains type definitions for all commands used by the extension.
@@ -13,6 +21,38 @@ export type BaseCommands = {
   "codeQL.openDocumentation": () => Promise<void>;
 };
 
+// Commands used for the query history panel
+export type QueryHistoryCommands = {
+  // Commands in the "navigation" group
+  "codeQLQueryHistory.sortByName": () => Promise<void>;
+  "codeQLQueryHistory.sortByDate": () => Promise<void>;
+  "codeQLQueryHistory.sortByCount": () => Promise<void>;
+
+  // Commands in the context menu or in the hover menu
+  "codeQLQueryHistory.openQueryTitleMenu": SelectionCommandFunction<QueryHistoryInfo>;
+  "codeQLQueryHistory.openQueryContextMenu": SelectionCommandFunction<QueryHistoryInfo>;
+  "codeQLQueryHistory.removeHistoryItemTitleMenu": SelectionCommandFunction<QueryHistoryInfo>;
+  "codeQLQueryHistory.removeHistoryItemContextMenu": SelectionCommandFunction<QueryHistoryInfo>;
+  "codeQLQueryHistory.removeHistoryItemContextInline": SelectionCommandFunction<QueryHistoryInfo>;
+  "codeQLQueryHistory.renameItem": SelectionCommandFunction<QueryHistoryInfo>;
+  "codeQLQueryHistory.compareWith": SelectionCommandFunction<QueryHistoryInfo>;
+  "codeQLQueryHistory.showEvalLog": SelectionCommandFunction<QueryHistoryInfo>;
+  "codeQLQueryHistory.showEvalLogSummary": SelectionCommandFunction<QueryHistoryInfo>;
+  "codeQLQueryHistory.showEvalLogViewer": SelectionCommandFunction<QueryHistoryInfo>;
+  "codeQLQueryHistory.showQueryLog": SelectionCommandFunction<QueryHistoryInfo>;
+  "codeQLQueryHistory.showQueryText": SelectionCommandFunction<QueryHistoryInfo>;
+  "codeQLQueryHistory.openQueryDirectory": SelectionCommandFunction<QueryHistoryInfo>;
+  "codeQLQueryHistory.cancel": SelectionCommandFunction<QueryHistoryInfo>;
+  "codeQLQueryHistory.exportResults": SelectionCommandFunction<QueryHistoryInfo>;
+  "codeQLQueryHistory.viewCsvResults": SelectionCommandFunction<QueryHistoryInfo>;
+  "codeQLQueryHistory.viewCsvAlerts": SelectionCommandFunction<QueryHistoryInfo>;
+  "codeQLQueryHistory.viewSarifAlerts": SelectionCommandFunction<QueryHistoryInfo>;
+  "codeQLQueryHistory.viewDil": SelectionCommandFunction<QueryHistoryInfo>;
+  "codeQLQueryHistory.itemClicked": SelectionCommandFunction<QueryHistoryInfo>;
+  "codeQLQueryHistory.openOnGithub": SelectionCommandFunction<QueryHistoryInfo>;
+  "codeQLQueryHistory.copyRepoList": SelectionCommandFunction<QueryHistoryInfo>;
+};
+
 // Commands tied to variant analysis
 export type VariantAnalysisCommands = {
   "codeQL.openVariantAnalysisLogs": (
@@ -22,6 +62,8 @@ export type VariantAnalysisCommands = {
   "codeQL.runVariantAnalysisContextEditor": (uri?: Uri) => Promise<void>;
 };
 
-export type AllCommands = BaseCommands & VariantAnalysisCommands;
+export type AllCommands = BaseCommands &
+  QueryHistoryCommands &
+  VariantAnalysisCommands;
 
 export type AppCommandManager = CommandManager<AllCommands>;
