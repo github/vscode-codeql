ce413a638558086c53950687a29a998a9ded3110
Elena Tanasoiu <elenatanasoiu@github.com>
2023-03-21 08:56:14 +0000

    Stub `isCodespaceTemplate` correctly
    Now that we fixed our expectation in the previous commit, we could see we
    were stubbing this to false instead of true.
    
    So now the test is checking the right scenario.

--- extensions/ql-vscode/test/vscode-tests/no-workspace/helpers.test.ts
+++ extensions/ql-vscode/test/vscode-tests/no-workspace/helpers.test.ts
@@ -620,7 +620,7 @@ describe("prepareCodeTour", () => {
     describe("if the workspace is already open", () => {
       it("should not open the tutorial workspace", async () => {
         // Set isCodespaceTemplate to true to indicate the workspace has already been opened
-        jest.spyOn(Setting.prototype, "getValue").mockReturnValue(false);
+        jest.spyOn(Setting.prototype, "getValue").mockReturnValue(true);
 
         // set up directory to have a 'tutorial.code-workspace' file
         const tutorialWorkspacePath = join(dir.name, "tutorial.code-workspace");
