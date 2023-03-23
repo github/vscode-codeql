d5fd39f9f801f9e318b4ef3d482a9ad81840c75c
Robert <robertbrignull@github.com>
2023-03-23 16:55:40 +0000

    Convert cleanDatabases to not use commands

--- extensions/ql-vscode/src/local-databases.ts
+++ extensions/ql-vscode/src/local-databases.ts
@@ -1001,6 +1001,15 @@ export class DatabaseManager extends DisposableObject {
     });
   }
 
+  public async removeAllDatabases(
+    progress: ProgressCallback,
+    token: vscode.CancellationToken,
+  ) {
+    for (const item of this.databaseItems) {
+      await this.removeDatabaseItem(progress, token, item);
+    }
+  }
+
   private async deregisterDatabase(
     progress: ProgressCallback,
     token: vscode.CancellationToken,
