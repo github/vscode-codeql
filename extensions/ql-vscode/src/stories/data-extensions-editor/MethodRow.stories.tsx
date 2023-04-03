3ae16d95340985918644d937326e669885e017f6
Koen Vlaswinkel <koesie10@github.com>
2023-04-03 14:46:30 +0200

    Add story for MethodRow

---
+++ extensions/ql-vscode/src/stories/data-extensions-editor/MethodRow.stories.tsx
@@ -0,0 +1,54 @@
+import * as React from "react";
+
+import { ComponentMeta, ComponentStory } from "@storybook/react";
+
+import { MethodRow as MethodRowComponent } from "../../view/data-extensions-editor/MethodRow";
+
+export default {
+  title: "Data Extensions Editor/Method Row",
+  component: MethodRowComponent,
+} as ComponentMeta<typeof MethodRowComponent>;
+
+const Template: ComponentStory<typeof MethodRowComponent> = (args) => (
+  <MethodRowComponent {...args} />
+);
+
+export const MethodRow = Template.bind({});
+MethodRow.args = {
+  externalApiUsage: {
+    signature: "org.sql2o.Sql2o#open()",
+    packageName: "org.sql2o",
+    typeName: "Sql2o",
+    methodName: "open",
+    methodParameters: "()",
+    supported: true,
+    usages: [
+      {
+        label: "open(...)",
+        url: {
+          uri: "file:/home/runner/work/sql2o-example/sql2o-example/src/main/java/org/example/HelloController.java",
+          startLine: 14,
+          startColumn: 24,
+          endLine: 14,
+          endColumn: 35,
+        },
+      },
+      {
+        label: "open(...)",
+        url: {
+          uri: "file:/home/runner/work/sql2o-example/sql2o-example/src/main/java/org/example/HelloController.java",
+          startLine: 25,
+          startColumn: 24,
+          endLine: 25,
+          endColumn: 35,
+        },
+      },
+    ],
+  },
+  modeledMethod: {
+    type: "summary",
+    input: "Argument[-1]",
+    output: "ReturnValue",
+    kind: "taint",
+  },
+};
