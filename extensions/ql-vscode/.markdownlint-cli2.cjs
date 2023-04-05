56b36baa8210f6ddf3d69431ea5eb9f5e685a00a
Sam Partington <sampart@github.com>
2023-04-05 10:25:55 +0100

    Move config file to right place in relation to node_modules
    https://github.com/github/vscode-codeql/pull/2268#pullrequestreview-1371042057
    
    Still need to consider how to ensure that the extension picks
    up the right config.

--- extensions/ql-vscode/.markdownlint-cli2.cjs
+++ extensions/ql-vscode/.markdownlint-cli2.cjs
@@ -8,4 +8,4 @@ module.exports = {
     outputFormatters: [
       [ "markdownlint-cli2-formatter-pretty", { "appendLink": true } ] // ensures the error message includes a link to the rule documentation
     ]
-}
+} 
