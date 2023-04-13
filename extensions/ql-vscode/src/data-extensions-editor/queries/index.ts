0a304f633a7f2be35182f962e434eb5c6a1540b8
Koen Vlaswinkel <koesie10@github.com>
2023-04-13 15:01:43 +0200

    Use enum for query texts

--- extensions/ql-vscode/src/data-extensions-editor/queries/index.ts
+++ extensions/ql-vscode/src/data-extensions-editor/queries/index.ts
@@ -1,6 +1,7 @@
 import { fetchExternalApisQuery as javaFetchExternalApisQuery } from "./java";
 import { Query } from "./query";
+import { QueryLanguage } from "../../common/query-language";
 
-export const fetchExternalApiQueries: Record<string, Query> = {
-  java: javaFetchExternalApisQuery,
+export const fetchExternalApiQueries: Partial<Record<QueryLanguage, Query>> = {
+  [QueryLanguage.Java]: javaFetchExternalApisQuery,
 };
