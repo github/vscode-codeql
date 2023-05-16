1ceccc8b4dcf2540a46334c88b422f9a8e1d478c
Robert <robertbrignull@github.com>
2023-05-16 16:57:02 +0100

    Avoid double-declaring fields that are inherited from TreeItem

--- extensions/ql-vscode/src/queries-panel/query-tree-view-item.ts
+++ extensions/ql-vscode/src/queries-panel/query-tree-view-item.ts
@@ -1,13 +1,13 @@
 import * as vscode from "vscode";
 
 export class QueryTreeViewItem extends vscode.TreeItem {
-  public collapsibleState: vscode.TreeItemCollapsibleState;
   constructor(
-    public readonly label: string,
-    public readonly tooltip: string | undefined,
+    label: string,
+    tooltip: string | undefined,
     public readonly children: QueryTreeViewItem[],
   ) {
     super(label);
+    this.tooltip = tooltip;
     this.collapsibleState = this.children.length
       ? vscode.TreeItemCollapsibleState.Collapsed
       : vscode.TreeItemCollapsibleState.None;
