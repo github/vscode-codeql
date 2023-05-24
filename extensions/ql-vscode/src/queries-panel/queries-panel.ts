9dee2a132e348d2f6d6ff07baa81792655fda3b3
Robert <robertbrignull@github.com>
2023-05-24 10:37:22 +0100

    Remove member variables that could be local variables

--- extensions/ql-vscode/src/queries-panel/queries-panel.ts
+++ extensions/ql-vscode/src/queries-panel/queries-panel.ts
@@ -1,22 +1,17 @@
 import * as vscode from "vscode";
 import { DisposableObject } from "../pure/disposable-object";
 import { QueryTreeDataProvider } from "./query-tree-data-provider";
-import { QueryTreeViewItem } from "./query-tree-view-item";
 import { QueryDiscovery } from "./query-discovery";
 
 export class QueriesPanel extends DisposableObject {
-  private readonly dataProvider: QueryTreeDataProvider;
-  private readonly treeView: vscode.TreeView<QueryTreeViewItem>;
-
   public constructor(queryDiscovery: QueryDiscovery) {
     super();
 
-    this.dataProvider = new QueryTreeDataProvider(queryDiscovery);
+    const dataProvider = new QueryTreeDataProvider(queryDiscovery);
 
-    this.treeView = vscode.window.createTreeView("codeQLQueries", {
-      treeDataProvider: this.dataProvider,
+    const treeView = vscode.window.createTreeView("codeQLQueries", {
+      treeDataProvider: dataProvider,
     });
-
-    this.push(this.treeView);
+    this.push(treeView);
   }
 }
