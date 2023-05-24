c004f187208bb848ceb96cd45fe16cfc6565c3cf
Koen Vlaswinkel <koesie10@github.com>
2023-05-24 17:04:36 +0200

    Remove outdated comment

--- extensions/ql-vscode/src/databases/local-databases/index.ts
+++ extensions/ql-vscode/src/databases/local-databases/index.ts
@@ -3,13 +3,3 @@ export { DatabaseChangedEvent, DatabaseEventKind } from "./database-events";
 export { DatabaseItem } from "./database-item";
 export { DatabaseManager } from "./database-manager";
 export { DatabaseResolver } from "./database-resolver";
-
-/**
- * databases.ts
- * ------------
- * Managing state of what the current database is, and what other
- * databases have been recently selected.
- *
- * The source of truth of the current state resides inside the
- * `DatabaseManager` class below.
- */
