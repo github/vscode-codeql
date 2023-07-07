5eab1f8882b98c45e2ad4b88246a579daee5aa01
Robert <robertbrignull@github.com>
2023-07-07 17:07:01 +0100

    Add documentation to ExternalApiUsage fields

--- extensions/ql-vscode/src/data-extensions-editor/external-api-usage.ts
+++ extensions/ql-vscode/src/data-extensions-editor/external-api-usage.ts
@@ -11,13 +11,24 @@ export type ExternalApiUsage = {
    */
   library: string;
   /**
-   * Contains the full method signature, e.g. `org.sql2o.Connection#createQuery(String)`
+   * A unique signature that can be used to identify this external API usage.
+   *
+   * The signature contains the package name, type name, method name, and method parameters
+   * in the form "packageName.typeName#methodName(methodParameters)".
+   * e.g. `org.sql2o.Connection#createQuery(String)`
    */
   signature: string;
   packageName: string;
   typeName: string;
   methodName: string;
+  /**
+   * The method parameters, including enclosing parentheses, e.g. `(String, String)`
+   */
   methodParameters: string;
+  /**
+   * Is this method already supported by CodeQL standard libraries.
+   * If so, there is no need for the user to model it themselves.
+   */
   supported: boolean;
   usages: Call[];
 };
