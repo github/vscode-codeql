b18b3e575df93f56e68f78eb9c8604b5f4a2553e
Dave Bartolomeo <dbartol@github.com>
2023-07-07 16:33:07 -0400

    Periodic screenshots step

--- extensions/ql-vscode/screenlog.ps1
+++ extensions/ql-vscode/screenlog.ps1
@@ -1,8 +1,9 @@
 Write-Host "Starting to take periodic screenshots..."
 for ($Elapsed = 0; $Elapsed -lt (20 * 60); $Elapsed += 30)
 {
-    Write-Host "Taking screenshot at ${Elapsed} seconds ('elapsed_${Elapsed}.png')"
-    .\screenshot.bat "elapsed_${Elapsed}.png"
+    $T = Get-Date -Format "yyyy-MM-dd-HH-mm-ss"
+    Write-Host "Taking screenshot at ${Elapsed} seconds ('elapsed_${T}.png')"
+    .\screenshot.bat "elapsed_${T}.png"
     Write-Host "Sleeping for 30 seconds..."
     Start-Sleep -Seconds 30
 }
