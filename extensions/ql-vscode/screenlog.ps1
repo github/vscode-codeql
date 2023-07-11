6b2373de64ef491c11ce962950a962615c699fe9
Dave Bartolomeo <dbartol@github.com>
2023-07-11 14:36:58 -0400

    Delete unneeded files

--- extensions/ql-vscode/screenlog.ps1
+++
@@ -1,9 +0,0 @@
-Write-Host "Starting to take periodic screenshots..."
-for ($Elapsed = 0; $Elapsed -lt (20 * 60); $Elapsed += 30)
-{
-    $T = Get-Date -Format "yyyy-MM-dd-HH-mm-ss"
-    Write-Host "Taking screenshot at ${Elapsed} seconds ('elapsed_${T}.png')"
-    .\screenshot.bat "elapsed_${T}.png"
-    Write-Host "Sleeping for 30 seconds..."
-    Start-Sleep -Seconds 30
-}
