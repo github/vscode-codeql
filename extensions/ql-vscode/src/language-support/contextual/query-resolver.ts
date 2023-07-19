57bcfbbe29605fe63aa9ee713839b9016d52d14d
Koen Vlaswinkel <koesie10@github.com>
2023-07-19 14:29:34 +0200

    Extract creation of lock file to more generic function

--- extensions/ql-vscode/src/language-support/contextual/query-resolver.ts
+++ extensions/ql-vscode/src/language-support/contextual/query-resolver.ts
@@ -1,6 +1,3 @@
-import { promises } from "fs-extra";
-import { basename, dirname, resolve } from "path";
-
 import { getOnDiskWorkspaceFolders } from "../../common/vscode/workspace-folders";
 import { QlPacksForLanguage } from "../../databases/qlpack";
 import {
@@ -17,7 +14,7 @@ import { TeeLogger } from "../../common/logging";
 import { CancellationToken } from "vscode";
 import { ProgressCallback } from "../../common/vscode/progress";
 import { CoreCompletedQuery, QueryRunner } from "../../query-server";
-import { QLPACK_FILENAMES } from "../../common/ql";
+import { createLockFileForStandardQuery } from "../../local-queries/standard-queries";
 
 export async function resolveQueries(
   cli: CodeQLCliServer,
@@ -27,64 +24,6 @@ export async function resolveQueries(
   return resolveLocalQueries(cli, qlpacks, nameOfKeyType(keyType), {
     kind: kindOfKeyType(keyType),
     "tags contain": [tagOfKeyType(keyType)],
-  });
-}
-
-async function resolveContextualQuery(
-  cli: CodeQLCliServer,
-  query: string,
-): Promise<{ packPath: string; createdTempLockFile: boolean }> {
-  // Contextual queries now live within the standard library packs.
-  // This simplifies distribution (you don't need the standard query pack to use the AST viewer),
-  // but if the library pack doesn't have a lockfile, we won't be able to find
-  // other pack dependencies of the library pack.
-
-  // Work out the enclosing pack.
-  const packContents = await cli.packPacklist(query, false);
-  const packFilePath = packContents.find((p) =>
-    QLPACK_FILENAMES.includes(basename(p)),
-  );
-  if (packFilePath === undefined) {
-    // Should not happen; we already resolved this query.
-    throw new Error(
-      `Could not find a CodeQL pack file for the pack enclosing the contextual query ${query}`,
-    );
-  }
-  const packPath = dirname(packFilePath);
-  const lockFilePath = packContents.find((p) =>
-    ["codeql-pack.lock.yml", "qlpack.lock.yml"].includes(basename(p)),
-  );
-  let createdTempLockFile = false;
-  if (!lockFilePath) {
-    // No lock file, likely because this library pack is in the package cache.
-    // Create a lock file so that we can resolve dependencies and library path
-    // for the contextual query.
-    void extLogger.log(
-      `Library pack ${packPath} is missing a lock file; creating a temporary lock file`,
-    );
-    await cli.packResolveDependencies(packPath);
-    createdTempLockFile = true;
-    // Clear CLI server pack cache before installing dependencies,
-    // so that it picks up the new lock file, not the previously cached pack.
-    void extLogger.log("Clearing the CodeQL CLI server's pack cache");
-    await cli.clearCache();
-    // Install dependencies.
-    void extLogger.log(
-      `Installing package dependencies for library pack ${packPath}`,
-    );
-    await cli.packInstall(packPath);
-  }
-  return { packPath, createdTempLockFile };
-}
-
-async function removeTemporaryLockFile(packPath: string) {
-  const tempLockFilePath = resolve(packPath, "codeql-pack.lock.yml");
-  void extLogger.log(
-    `Deleting temporary package lock file at ${tempLockFilePath}`,
-  );
-  // It's fine if the file doesn't exist.
-  await promises.rm(resolve(packPath, "codeql-pack.lock.yml"), {
-    force: true,
   });
 }
 
@@ -98,10 +37,7 @@ export async function runContextualQuery(
   token: CancellationToken,
   templates: Record<string, string>,
 ): Promise<CoreCompletedQuery> {
-  const { packPath, createdTempLockFile } = await resolveContextualQuery(
-    cli,
-    query,
-  );
+  const { cleanup } = await createLockFileForStandardQuery(cli, query);
   const queryRun = qs.createQueryRun(
     db.databaseUri.fsPath,
     { queryPath: query, quickEvalPosition: undefined },
@@ -120,8 +56,6 @@ export async function runContextualQuery(
     token,
     new TeeLogger(qs.logger, queryRun.outputDir.logPath),
   );
-  if (createdTempLockFile) {
-    await removeTemporaryLockFile(packPath);
-  }
+  await cleanup?.();
   return results;
 }
---
+++ extensions/ql-vscode/src/local-queries/standard-queries.ts
@@ -0,0 +1,77 @@
+import { CodeQLCliServer } from "../codeql-cli/cli";
+import { QLPACK_FILENAMES, QLPACK_LOCK_FILENAMES } from "../common/ql";
+import { basename, dirname, resolve } from "path";
+import { extLogger } from "../common/logging/vscode";
+import { promises } from "fs-extra";
+import { BaseLogger } from "../common/logging";
+
+export type LockFileForStandardQueryResult = {
+  cleanup?: () => Promise<void>;
+};
+
+/**
+ * Create a temporary query suite for a given query living within the standard library packs.
+ *
+ * This will create a lock file so the CLI can run the query without having the ql submodule.
+ */
+export async function createLockFileForStandardQuery(
+  cli: CodeQLCliServer,
+  queryPath: string,
+  logger: BaseLogger = extLogger,
+): Promise<LockFileForStandardQueryResult> {
+  // These queries live within the standard library packs.
+  // This simplifies distribution (you don't need the standard query pack to use the AST viewer),
+  // but if the library pack doesn't have a lockfile, we won't be able to find
+  // other pack dependencies of the library pack.
+
+  // Work out the enclosing pack.
+  const packContents = await cli.packPacklist(queryPath, false);
+  const packFilePath = packContents.find((p) =>
+    QLPACK_FILENAMES.includes(basename(p)),
+  );
+  if (packFilePath === undefined) {
+    // Should not happen; we already resolved this query.
+    throw new Error(
+      `Could not find a CodeQL pack file for the pack enclosing the contextual query ${queryPath}`,
+    );
+  }
+  const packPath = dirname(packFilePath);
+  const lockFilePath = packContents.find((p) =>
+    QLPACK_LOCK_FILENAMES.includes(basename(p)),
+  );
+
+  let cleanup: (() => Promise<void>) | undefined = undefined;
+
+  if (!lockFilePath) {
+    // No lock file, likely because this library pack is in the package cache.
+    // Create a lock file so that we can resolve dependencies and library path
+    // for the contextual query.
+    void logger.log(
+      `Library pack ${packPath} is missing a lock file; creating a temporary lock file`,
+    );
+    await cli.packResolveDependencies(packPath);
+
+    cleanup = async () => {
+      const tempLockFilePath = resolve(packPath, "codeql-pack.lock.yml");
+      void logger.log(
+        `Deleting temporary package lock file at ${tempLockFilePath}`,
+      );
+      // It's fine if the file doesn't exist.
+      await promises.rm(resolve(packPath, "codeql-pack.lock.yml"), {
+        force: true,
+      });
+    };
+
+    // Clear CLI server pack cache before installing dependencies,
+    // so that it picks up the new lock file, not the previously cached pack.
+    void logger.log("Clearing the CodeQL CLI server's pack cache");
+    await cli.clearCache();
+    // Install dependencies.
+    void logger.log(
+      `Installing package dependencies for library pack ${packPath}`,
+    );
+    await cli.packInstall(packPath);
+  }
+
+  return { cleanup };
+}
