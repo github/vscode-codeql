1e6b7a6619a5d7686b42d073fdafd4c68699a09d
Anders Starcke Henriksen <starcke@github.com>
2023-09-26 16:16:28 +0200

    Add support for filtering queries panel.

--- extensions/ql-vscode/src/queries-panel/queries-module.ts
+++ extensions/ql-vscode/src/queries-panel/queries-module.ts
@@ -6,6 +6,7 @@ import { DisposableObject } from "../common/disposable-object";
 import { QueriesPanel } from "./queries-panel";
 import { QueryDiscovery } from "./query-discovery";
 import { QueryPackDiscovery } from "./query-pack-discovery";
+import { LanguageContextStore } from "../language-context-store";
 
 export class QueriesModule extends DisposableObject {
   private queriesPanel: QueriesPanel | undefined;
@@ -16,16 +17,21 @@ export class QueriesModule extends DisposableObject {
 
   public static initialize(
     app: App,
+    languageContext: LanguageContextStore,
     cliServer: CodeQLCliServer,
   ): QueriesModule {
     const queriesModule = new QueriesModule(app);
     app.subscriptions.push(queriesModule);
 
-    queriesModule.initialize(app, cliServer);
+    queriesModule.initialize(app, languageContext, cliServer);
     return queriesModule;
   }
 
-  private initialize(app: App, cliServer: CodeQLCliServer): void {
+  private initialize(
+    app: App,
+    langauageContext: LanguageContextStore,
+    cliServer: CodeQLCliServer,
+  ): void {
     // Currently, we only want to expose the new panel when we are in canary mode
     // and the user has enabled the "Show queries panel" flag.
     if (!isCanary() || !showQueriesPanel()) {
@@ -38,8 +44,9 @@ export class QueriesModule extends DisposableObject {
     void queryPackDiscovery.initialRefresh();
 
     const queryDiscovery = new QueryDiscovery(
-      app.environment,
+      app,
       queryPackDiscovery,
+      langauageContext,
     );
     this.push(queryDiscovery);
     void queryDiscovery.initialRefresh();
