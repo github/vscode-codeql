31320d4a8c830b4b7e0574b77fa2e47760cd4bac
Charis Kyriakou <charisk@users.noreply.github.com>
2023-10-06 07:52:10 +0000

    Add stories for new components

---
+++ extensions/ql-vscode/src/stories/method-modeling/NoMethodSelected.stories.tsx
@@ -0,0 +1,16 @@
+import * as React from "react";
+
+import { Meta, StoryFn } from "@storybook/react";
+
+import { NoMethodSelected as NoMethodSelectedComponent } from "../../view/method-modeling/NoMethodSelected";
+
+export default {
+  title: "Method Modeling/No Method Selected",
+  component: NoMethodSelectedComponent,
+} as Meta<typeof NoMethodSelectedComponent>;
+
+const Template: StoryFn<typeof NoMethodSelectedComponent> = () => (
+  <NoMethodSelectedComponent />
+);
+
+export const NoMethodSelected = Template.bind({});
