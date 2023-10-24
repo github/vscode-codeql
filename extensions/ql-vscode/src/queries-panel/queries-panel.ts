1e58e5a723667b7d3eb36d5d59cccea91fc7b693
Koen Vlaswinkel <koesie10@github.com>
2023-10-24 15:52:20 +0200

    Reveal opened file in queries panel

--- extensions/ql-vscode/src/queries-panel/queries-panel.ts
+++ extensions/ql-vscode/src/queries-panel/queries-panel.ts
@@ -17,5 +17,31 @@ export class QueriesPanel extends DisposableObject {
       treeDataProvider: dataProvider,
     });
     this.push(treeView);
+
+    window.onDidChangeActiveTextEditor((textEditor) => {
+      if (!textEditor) {
+        return;
+      }
+
+      const filePath = textEditor.document.uri.fsPath;
+
+      const item = dataProvider.getTreeItemByPath(filePath);
+      if (!item) {
+        return;
+      }
+
+      if (
+        treeView.selection.length === 1 &&
+        treeView.selection[0].path === item.path
+      ) {
+        // The item is already selected
+        return;
+      }
+
+      void treeView.reveal(item, {
+        select: true,
+        focus: false,
+      });
+    });
   }
 }
