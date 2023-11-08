bdc7a692ec4def77c510dc8e604e23d59d9bdd50
Koen Vlaswinkel <koesie10@github.com>
2023-11-08 12:06:42 +0100

    Improve parsing of partial access paths

---
+++ extensions/ql-vscode/src/view/common/SuggestBox/__tests__/access-path.test.ts
@@ -0,0 +1,71 @@
+import { hasAccessPathSyntaxError, parseAccessPathParts } from "../access-path";
+
+describe("parseAccessPathParts", () => {
+  it.each([
+    {
+      path: "Argument[foo].Element.Field[@test]",
+      parts: ["Argument[foo]", "Element", "Field[@test]"],
+    },
+    {
+      path: "Argument[foo].Element.Field[foo.Bar.x]",
+      parts: ["Argument[foo]", "Element", "Field[foo.Bar.x]"],
+    },
+    {
+      path: "Argument[",
+      parts: ["Argument["],
+    },
+    {
+      path: "Argument[se",
+      parts: ["Argument[se"],
+    },
+    {
+      path: "Argument[foo].Field[",
+      parts: ["Argument[foo]", "Field["],
+    },
+    {
+      path: "Argument[foo].",
+      parts: ["Argument[foo]", ""],
+    },
+    {
+      path: "Argument[foo]..",
+      parts: ["Argument[foo]", "", ""],
+    },
+  ])(`parses correctly for $path`, ({ path, parts }) => {
+    expect(parseAccessPathParts(path)).toEqual(parts);
+  });
+});
+
+describe("hasAccessPathSyntaxError", () => {
+  it.each([
+    {
+      path: "Argument[foo].Element.Field[@test]",
+      valid: true,
+    },
+    {
+      path: "Argument[foo].Element.Field[foo.Bar.x]",
+      valid: true,
+    },
+    {
+      path: "Argument[",
+      valid: false,
+    },
+    {
+      path: "Argument[se",
+      valid: false,
+    },
+    {
+      path: "Argument[foo].Field[",
+      valid: false,
+    },
+    {
+      path: "Argument[foo].",
+      valid: false,
+    },
+    {
+      path: "Argument[foo]..",
+      valid: false,
+    },
+  ])(`validates $path correctly`, ({ path, valid }) => {
+    expect(hasAccessPathSyntaxError(path)).toEqual(!valid);
+  });
+});
--- extensions/ql-vscode/src/view/common/SuggestBox/access-path.ts
+++ extensions/ql-vscode/src/view/common/SuggestBox/access-path.ts
@@ -1,22 +1,36 @@
-// Based on https://github.com/github/codeql/blob/a04830b8b2d3e5f7df8e1f80f06c020b987a89a3/ruby/ql/lib/codeql/ruby/dataflow/internal/AccessPathSyntax.qll
-
 // Avoid splitting by '.' since tokens may contain dots, e.g. `Field[foo.Bar.x]`.
-// Instead use regexpFind to match valid tokens, and supplement with a final length
-// check (in `AccessPath.hasSyntaxError`) to ensure all characters were included in a token.
-const tokenRegex = /\w+(?:\[[^\]]*])?(?=\.|$)/;
+// Instead use some simple parsing to match valid tokens.
+// Based on https://github.com/github/codeql/blob/a04830b8b2d3e5f7df8e1f80f06c020b987a89a3/ruby/ql/lib/codeql/ruby/dataflow/internal/AccessPathSyntax.qll
+// However, this does not use a regex to be more lenient in parsing (e.g. parsing partial access paths)
 
 export function parseAccessPathParts(path: string): string[] {
   const parts: string[] = [];
-  let match: RegExpExecArray | null;
-  while ((match = tokenRegex.exec(path))) {
-    parts.push(match[0]);
-    path = path.slice(match.index + match[0].length);
+
+  let currentPart = "";
+  let inPath = false;
+  for (let i = 0; i < path.length; i++) {
+    const c = path[i];
+
+    if (c === "[") {
+      inPath = true;
+    } else if (c === "]") {
+      inPath = false;
+    } else if (c === "." && !inPath) {
+      parts.push(currentPart);
+      currentPart = "";
+      continue;
+    }
+
+    currentPart += c;
   }
-  if (path.startsWith(".")) {
-    parts.push(path.slice(1));
-  }
+
+  parts.push(currentPart);
+
   return parts;
 }
+
+// Regex for a single part of the access path
+const tokenRegex = /^(\w+)(?:\[([^\]]*)])?$/;
 
 export function hasAccessPathSyntaxError(path: string): boolean {
   if (path === "") {
@@ -27,5 +41,9 @@ export function hasAccessPathSyntaxError(path: string): boolean {
   const totalExpectedLength =
     parts.reduce((sum, part) => sum + part.length + 1, 0) - 1;
 
-  return totalExpectedLength !== path.length;
+  if (totalExpectedLength !== path.length) {
+    return false;
+  }
+
+  return parts.some((part) => !tokenRegex.test(part));
 }
