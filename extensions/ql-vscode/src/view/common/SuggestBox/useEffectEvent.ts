de38464a0b48621d010f9b4c0c5318989e297607
Koen Vlaswinkel <koesie10@github.com>
2023-11-08 13:26:19 +0100

    Simplify useEffectEvent

--- extensions/ql-vscode/src/view/common/SuggestBox/useEffectEvent.ts
+++ extensions/ql-vscode/src/view/common/SuggestBox/useEffectEvent.ts
@@ -4,6 +4,13 @@ type AnyFunction = (...args: any[]) => any;
 
 // Copy of https://github.com/floating-ui/floating-ui/blob/a70e4e2350627ba61bdfd72f86475d0905c2a17e/packages/react/src/hooks/utils/useEffectEvent.ts
 // since it's not exported
+
+/**
+ * Creates a reference to a callback that will never change in value. This will ensure that when a callback gets changed,
+ * no new reference to the callback will be created and thus no unnecessary re-renders will be triggered.
+ *
+ * @param callback The callback to call when the event is triggered.
+ */
 export function useEffectEvent<T extends AnyFunction>(callback?: T) {
   const ref = useRef<AnyFunction | undefined>(() => {});
 
