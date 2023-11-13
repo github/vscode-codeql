6273f7eebe955c3cbad238a9a0f9de0aa867e48f
shati-patel <42641846+shati-patel@users.noreply.github.com>
2023-11-13 11:30:13 +0000

    Remove feature flags and ship queries panel + language selector

--- extensions/ql-vscode/src/queries-panel/queries-module.ts
+++ extensions/ql-vscode/src/queries-panel/queries-module.ts
@@ -1,7 +1,6 @@
 import { CodeQLCliServer } from "../codeql-cli/cli";
 import { extLogger } from "../common/logging/vscode";
 import { App } from "../common/app";
-import { isCanary, showQueriesPanel } from "../config";
 import { DisposableObject } from "../common/disposable-object";
 import { QueriesPanel } from "./queries-panel";
 import { QueryDiscovery } from "./query-discovery";
@@ -41,11 +40,6 @@ export class QueriesModule extends DisposableObject {
     langauageContext: LanguageContextStore,
     cliServer: CodeQLCliServer,
   ): void {
-    // Currently, we only want to expose the new panel when we are in canary mode
-    // and the user has enabled the "Show queries panel" flag.
-    if (!isCanary() || !showQueriesPanel()) {
-      return;
-    }
     void extLogger.log("Initializing queries panel.");
 
     const queryPackDiscovery = new QueryPackDiscovery(cliServer);
