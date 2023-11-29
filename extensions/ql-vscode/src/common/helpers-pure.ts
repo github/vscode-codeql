443850538a1163ae2a283f7d59bf2ffe29785be7
Anders Starcke Henriksen <starcke@github.com>
2023-11-29 20:56:40 +0100

    Implement hash function.

--- extensions/ql-vscode/src/common/helpers-pure.ts
+++ extensions/ql-vscode/src/common/helpers-pure.ts
@@ -5,6 +5,7 @@
  * Helper functions that don't depend on vscode or the CLI and therefore can be used by the front-end and pure unit tests.
  */
 
+import { DataRow } from "../model-editor/model-extension-file";
 import { RedactableError } from "./errors";
 
 /**
@@ -89,4 +90,65 @@ function isChildProcessError(e: unknown): e is ChildProcessError {
     "stderr" in e &&
     typeof e.stderr === "string"
   );
+}
+
+/*
+  private int djb2_part(string s, int i) {
+    djb2_input(s) and i = 0 and result = 5381
+    or
+    (djb2_part(s, i - 1) * 33).bitXor(s.codePointAt(i - 1)) = result
+  }
+  
+  private int djb2(string s) {
+    // Bernstein hash (XOR version)
+    // seed = 5381
+    // hash(i) = hash(i - 1) * 33 ^ str[i]
+    result = djb2_part(s, s.length())
+  }*/
+
+export function djb2(s: string): number {
+  let hash = 5381;
+  for (let i = 0; i < s.length; i++) {
+    hash = ((hash << 5) + hash) ^ s.charCodeAt(i);
+  }
+  return hash;
+}
+
+/*+  exists(int h1, int h2, int h3, int h4, int h5, int h6, int h7, int h8, int h9, int h10, int p |
++    p = 19 and
++    madid =
++      ((((((((h1 * p + h2) * p + h3) * p + h4) * p + h5) * p + h6) * p + h7) * p + h8) * p + h9) * p
++        + h10 and
++    h1 = djb2(package) and
++    h2 = djb2(type) and
++    (
++      subtypes = true and h3 = 1
++      or
++      subtypes = false and h3 = 0
++    ) and
++    h4 = djb2(name) and
++    h5 = djb2(signature) and
++    h6 = djb2(ext) and
++    h7 = djb2(input) and
++    h8 = djb2(output) and
++    h9 = djb2(kind) and
++    h10 = djb2(provenance)
+*/
+
+export function hashMad(row: DataRow): string {
+  const p = 19;
+  let madid = 0;
+  for (const tuple of row) {
+    if (typeof tuple === "string") {
+      madid = madid * p + djb2(tuple);
+    } else if (typeof tuple === "number") {
+      madid = madid * p + tuple;
+    } else if (typeof tuple === "boolean") {
+      madid = madid * p + (tuple ? 1 : 0);
+    }
+    // We need to overflow the number to a 32-bit integer
+    madid = madid | 0;
+  }
+
+  return madid.toString();
 }
--- extensions/ql-vscode/test/unit-tests/common/helpers-pure.test.ts
+++ extensions/ql-vscode/test/unit-tests/common/helpers-pure.test.ts
@@ -1,4 +1,9 @@
-import { asyncFilter, getErrorMessage } from "../../../src/common/helpers-pure";
+import {
+  asyncFilter,
+  djb2,
+  getErrorMessage,
+  hashMad,
+} from "../../../src/common/helpers-pure";
 
 describe("helpers-pure", () => {
   it("should filter asynchronously", async () => {
@@ -17,5 +22,130 @@ describe("helpers-pure", () => {
     } catch (e) {
       expect(getErrorMessage(e)).toBe("opps");
     }
+  });
+
+  /*
+    	[empty string]	5,381
+2	java.util	-1,445,246,957
+3	run	193,431,916
+4	getName	-1,502,699,660
+5	clone	176,907,662
+6	main	2,087,696,334
+7	java.lang	-1,444,439,341
+8	start	195,030,917
+9	loadChildren	-1,971,081,232
+10	init	2,087,826,463
+11	getDescription	757,227,477
+12	toString	1,741,583,947
+*/
+  it("should hash single values properly", () => {
+    expect(djb2("")).toBe(5381);
+    expect(djb2("java.util")).toBe(-1445246957);
+    expect(djb2("run")).toBe(193431916);
+    expect(djb2("getName")).toBe(-1502699660);
+    expect(djb2("clone")).toBe(176907662);
+    expect(djb2("main")).toBe(2087696334);
+    expect(djb2("java.lang")).toBe(-1444439341);
+    expect(djb2("start")).toBe(195030917);
+    expect(djb2("loadChildren")).toBe(-1971081232);
+    expect(djb2("init")).toBe(2087826463);
+    expect(djb2("getDescription")).toBe(757227477);
+    expect(djb2("toString")).toBe(1741583947);
+  });
+
+  /*
+1	java.util	Enumeration	true	asIterator	[empty string]	[empty string]	Argument[this].Element	ReturnValue.Element	value	manual	1,679,884,592
+2	java.util	Enumeration	true	nextElement	[empty string]	[empty string]	Argument[this].Element	ReturnValue	value	manual	307,746,703
+3	java.util	Scanner	true	next	[empty string]	[empty string]	Argument[this]	ReturnValue	taint	manual	-1,602,956,821
+4	java.util	Scanner	true	reset	[empty string]	[empty string]	Argument[this]	ReturnValue	value	manual	-61,135,750
+5	java.util	Scanner	true	nextByte	[empty string]	[empty string]	Argument[this]	ReturnValue	taint	manual	1,270,097,313
+6	java.util	Scanner	true	skip	[empty string]	[empty string]	Argument[this]	ReturnValue	value	manual	1,643,621,326
+*/
+
+  it("should hash multiple values properly", () => {
+    expect(
+      hashMad([
+        "java.util",
+        "Enumeration",
+        true,
+        "asIterator",
+        "",
+        "",
+        "Argument[this].Element",
+        "ReturnValue.Element",
+        "value",
+        "manual",
+      ]),
+    ).toBe("1679884592");
+    expect(
+      hashMad([
+        "java.util",
+        "Enumeration",
+        true,
+        "nextElement",
+        "",
+        "",
+        "Argument[this].Element",
+        "ReturnValue",
+        "value",
+        "manual",
+      ]),
+    ).toBe("307746703");
+    expect(
+      hashMad([
+        "java.util",
+        "Scanner",
+        true,
+        "next",
+        "",
+        "",
+        "Argument[this]",
+        "ReturnValue",
+        "taint",
+        "manual",
+      ]),
+    ).toBe("-1602956821");
+    expect(
+      hashMad([
+        "java.util",
+        "Scanner",
+        true,
+        "reset",
+        "",
+        "",
+        "Argument[this]",
+        "ReturnValue",
+        "value",
+        "manual",
+      ]),
+    ).toBe("-61135750");
+    expect(
+      hashMad([
+        "java.util",
+        "Scanner",
+        true,
+        "nextByte",
+        "",
+        "",
+        "Argument[this]",
+        "ReturnValue",
+        "taint",
+        "manual",
+      ]),
+    ).toBe("1270097313");
+    expect(
+      hashMad([
+        "java.util",
+        "Scanner",
+        true,
+        "skip",
+        "",
+        "",
+        "Argument[this]",
+        "ReturnValue",
+        "value",
+        "manual",
+      ]),
+    ).toBe("1643621326");
   });
 });
