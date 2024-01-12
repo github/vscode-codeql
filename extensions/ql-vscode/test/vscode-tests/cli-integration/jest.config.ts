0534cb751453d1a1e72147baa77cf06ad5d35aef
Koen Vlaswinkel <koesie10@github.com>
2024-01-12 09:49:14 +0100

    Simplify custom Jest test runner
    The custom Jest test runner was originally written to install the
    required extensions for the CLI integration tests. This is no longer
    necessary as of https://github.com/github/vscode-codeql/pull/3232, so
    we can remove all code that deals with downloading VS Code and
    installing extensions. The download of VS Code will now be handled by
    the base `VSCodeTestRunner`.

--- extensions/ql-vscode/test/vscode-tests/cli-integration/jest.config.ts
+++ extensions/ql-vscode/test/vscode-tests/cli-integration/jest.config.ts
@@ -4,7 +4,7 @@ import baseConfig from "../jest.config.base";
 
 const config: Config = {
   ...baseConfig,
-  runner: "<rootDir>/../jest-runner-installed-extensions.ts",
+  runner: "<rootDir>/../jest-runner-vscode-codeql-cli.ts",
   setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
   // CLI integration tests call into the CLI and execute queries, so these are expected to take a lot longer
   // than the default 5 seconds.
