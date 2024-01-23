147c3514fa07d2a75f3efd2542725403886216f7
shati-patel <42641846+shati-patel@users.noreply.github.com>
2024-01-23 11:37:43 +0000

    Add new feature flag for "access path suggestions"
    Currently unused, but this will make it easier to add support for more complex access paths to the model editor.

--- extensions/ql-vscode/src/config.ts
+++ extensions/ql-vscode/src/config.ts
@@ -716,12 +716,17 @@ const LLM_GENERATION_DEV_ENDPOINT = new Setting(
 );
 const EXTENSIONS_DIRECTORY = new Setting("extensionsDirectory", MODEL_SETTING);
 const ENABLE_RUBY = new Setting("enableRuby", MODEL_SETTING);
+const ENABLE_ACCESS_PATH_SUGGESTIONS = new Setting(
+  "enableAccessPathSuggestions",
+  MODEL_SETTING,
+);
 
 export interface ModelConfig {
   flowGeneration: boolean;
   llmGeneration: boolean;
   getExtensionsDirectory(languageId: string): string | undefined;
   enableRuby: boolean;
+  enableAccessPathSuggestions: boolean;
 }
 
 export class ModelConfigListener extends ConfigListener implements ModelConfig {
@@ -761,6 +766,10 @@ export class ModelConfigListener extends ConfigListener implements ModelConfig {
 
   public get enableRuby(): boolean {
     return !!ENABLE_RUBY.getValue<boolean>();
+  }
+
+  public get enableAccessPathSuggestions(): boolean {
+    return !!ENABLE_ACCESS_PATH_SUGGESTIONS.getValue<boolean>();
   }
 }
 
