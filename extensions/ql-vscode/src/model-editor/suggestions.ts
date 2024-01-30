d0bb66e5d7a8a6b48cadb7b861884f5820c11efd
shati-patel <42641846+shati-patel@users.noreply.github.com>
2024-01-30 10:57:38 +0000

    Add types for complex access path suggestions

---
+++ extensions/ql-vscode/src/model-editor/suggestions.ts
@@ -0,0 +1,34 @@
+import type { MethodSignature } from "./method";
+
+export enum AccessPathSuggestionDefinitionType {
+  Array = "array",
+  Class = "class",
+  Enum = "enum",
+  EnumMember = "enum-member",
+  Field = "field",
+  Interface = "interface",
+  Key = "key",
+  Method = "method",
+  Misc = "misc",
+  Namespace = "namespace",
+  Parameter = "parameter",
+  Property = "property",
+  Structure = "structure",
+  Return = "return",
+  Variable = "variable",
+}
+
+export type AccessPathSuggestionRow = {
+  method: MethodSignature;
+  definitionType: AccessPathSuggestionDefinitionType;
+  value: string;
+  details: string;
+};
+
+export type AccessPathOption = {
+  label: string;
+  value: string;
+  icon: string;
+  details?: string;
+  followup?: AccessPathOption[];
+};
