b74306249e11f321d415def28f579074de361134
Koen Vlaswinkel <koesie10@github.com>
2024-02-07 13:41:29 +0100

    Fix not being able to open command palette in suggest box

--- extensions/ql-vscode/src/view/common/SuggestBox/SuggestBox.tsx
+++ extensions/ql-vscode/src/view/common/SuggestBox/SuggestBox.tsx
@@ -243,6 +243,7 @@ export const SuggestBox = <
               context={context}
               initialFocus={-1}
               visuallyHiddenDismiss
+              returnFocus={false}
             >
               <ListContainer
                 {...getFloatingProps({
