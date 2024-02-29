df782592bce43230cb01b9e439c755d7e267f015
Shati Patel <42641846+shati-patel@users.noreply.github.com>
2024-02-29 16:42:38 +0000

    Extract "ModelEvaluation" component (#3432)

---
+++ extensions/ql-vscode/src/view/model-editor/ModelEditorProgressRing.tsx
@@ -0,0 +1,8 @@
+import { VSCodeProgressRing } from "@vscode/webview-ui-toolkit/react";
+import { styled } from "styled-components";
+
+export const ModelEditorProgressRing = styled(VSCodeProgressRing)`
+  width: 16px;
+  height: 16px;
+  margin-right: 5px;
+`;
---
+++ extensions/ql-vscode/src/view/model-editor/ModelEvaluation.tsx
@@ -0,0 +1,53 @@
+import { VSCodeButton } from "@vscode/webview-ui-toolkit/react";
+import type { ModeledMethod } from "../../model-editor/modeled-method";
+import type { ModelEditorViewState } from "../../model-editor/shared/view-state";
+import type { ModelEvaluationRunState } from "../../model-editor/shared/model-evaluation-run-state";
+import { modelEvaluationRunIsRunning } from "../../model-editor/shared/model-evaluation-run-state";
+import { ModelEditorProgressRing } from "./ModelEditorProgressRing";
+
+type Props = {
+  viewState: ModelEditorViewState;
+  modeledMethods: Record<string, ModeledMethod[]>;
+  modifiedSignatures: Set<string>;
+  onStartEvaluation: () => void;
+  onStopEvaluation: () => void;
+  evaluationRun: ModelEvaluationRunState | undefined;
+};
+
+export const ModelEvaluation = ({
+  viewState,
+  modeledMethods,
+  modifiedSignatures,
+  onStartEvaluation,
+  onStopEvaluation,
+  evaluationRun,
+}: Props) => {
+  if (!viewState.showEvaluationUi) {
+    return null;
+  }
+
+  if (!evaluationRun || !modelEvaluationRunIsRunning(evaluationRun)) {
+    const customModelsExist = Object.values(modeledMethods).some(
+      (methods) => methods.filter((m) => m.type !== "none").length > 0,
+    );
+
+    const unsavedChanges = modifiedSignatures.size > 0;
+
+    return (
+      <VSCodeButton
+        onClick={onStartEvaluation}
+        appearance="secondary"
+        disabled={!customModelsExist || unsavedChanges}
+      >
+        Evaluate
+      </VSCodeButton>
+    );
+  } else {
+    return (
+      <VSCodeButton onClick={onStopEvaluation} appearance="secondary">
+        <ModelEditorProgressRing />
+        Stop evaluation
+      </VSCodeButton>
+    );
+  }
+};
