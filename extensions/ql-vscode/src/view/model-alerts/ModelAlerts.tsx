9a34c9c1ec8393b7a67ea0be9ceb598a393895bc
shati-patel <42641846+shati-patel@users.noreply.github.com>
2024-03-05 10:35:35 +0000

    Fix code scanning alert for "Missing origin verification in postMessage handler"

--- extensions/ql-vscode/src/view/model-alerts/ModelAlerts.tsx
+++ extensions/ql-vscode/src/view/model-alerts/ModelAlerts.tsx
@@ -3,9 +3,13 @@ import { useEffect } from "react";
 export function ModelAlerts(): React.JSX.Element {
   useEffect(() => {
     const listener = (evt: MessageEvent) => {
-      // sanitize origin
-      const origin = evt.origin.replace(/\n|\r/g, "");
-      console.error(`Invalid event origin ${origin}`);
+      if (evt.origin === window.origin) {
+        // TODO: handle messages
+      } else {
+        // sanitize origin
+        const origin = evt.origin.replace(/\n|\r/g, "");
+        console.error(`Invalid event origin ${origin}`);
+      }
     };
     window.addEventListener("message", listener);
 
