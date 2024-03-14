06f9f2c68725edf0f0d3f8ab63dd9889a595596d
Shati Patel <42641846+shati-patel@users.noreply.github.com>
2024-03-14 11:46:36 +0000

    Add title to model alerts view (#3465)

---
+++ extensions/ql-vscode/src/view/model-alerts/ModelAlertsHeader.tsx
@@ -0,0 +1,8 @@
+import type { ModelAlertsViewState } from "../../model-editor/shared/view-state";
+import { ViewTitle } from "../common";
+
+type Props = { viewState: ModelAlertsViewState };
+
+export const ModelAlertsHeader = ({ viewState }: Props) => {
+  return <ViewTitle>Model evaluation results for {viewState.title}</ViewTitle>;
+};
