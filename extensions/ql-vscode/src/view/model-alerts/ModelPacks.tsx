43d2b7f2aa524047b87988584f10aba046d6111e
Charis Kyriakou <charisk@users.noreply.github.com>
2024-03-14 09:47:06 +0000

    Add new ModelPacks component

---
+++ extensions/ql-vscode/src/view/model-alerts/ModelPacks.tsx
@@ -0,0 +1,43 @@
+import { styled } from "styled-components";
+import { LinkIconButton } from "../common/LinkIconButton";
+import type { ModelPackDetails } from "../../common/model-pack-details";
+
+const Title = styled.h3`
+  font-size: medium;
+  font-weight: 500;
+  margin: 0;
+`;
+
+const List = styled.ul`
+  list-style: none;
+  padding: 0;
+  margin-top: 5px;
+`;
+
+export const ModelPacks = ({
+  modelPacks,
+  openModelPackClick,
+}: {
+  modelPacks: ModelPackDetails[];
+  openModelPackClick: (path: string) => void;
+}): React.JSX.Element => {
+  if (modelPacks.length <= 0) {
+    return <></>;
+  }
+
+  return (
+    <>
+      <Title>Model packs</Title>
+      <List>
+        {modelPacks.map((modelPack) => (
+          <li key={modelPack.path}>
+            <LinkIconButton onClick={() => openModelPackClick(modelPack.path)}>
+              <span slot="start" className="codicon codicon-file-code"></span>
+              {modelPack.name}
+            </LinkIconButton>
+          </li>
+        ))}
+      </List>
+    </>
+  );
+};
