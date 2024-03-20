c553b1cb1d3336e4a41084a650339d113e8b5643
Koen Vlaswinkel <koesie10@github.com>
2024-03-20 12:08:36 +0100

    Run storybook migrate mdx-to-csf

--- extensions/ql-vscode/.storybook/main.ts
+++ extensions/ql-vscode/.storybook/main.ts
@@ -1,7 +1,7 @@
 import type { StorybookConfig } from "@storybook/react-webpack5";
 
 const config: StorybookConfig = {
-  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
