b3c83d175add31a1517874302a90895456931e2a
Koen Vlaswinkel <koesie10@github.com>
2024-03-26 12:09:51 +0100

    Update supported sink and source kinds

--- extensions/ql-vscode/src/model-editor/languages/csharp/index.ts
+++ extensions/ql-vscode/src/model-editor/languages/csharp/index.ts
@@ -10,6 +10,12 @@ export const csharp: ModelsAsDataLanguage = {
     },
     source: {
       ...staticLanguage.predicates.source,
+      supportedKinds: [
+        ...staticLanguage.predicates.source.supportedKinds,
+        // https://github.com/github/codeql/blob/0c5ea975a4c4dc5c439b908c006e440cb9bdf926/shared/mad/codeql/mad/ModelValidation.qll#L122-L123
+        "file-write",
+        "windows-registry",
+      ],
     },
   },
 };
