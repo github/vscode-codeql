3d4fa17394600a7789367a89f3d7f31284c76221
shati-patel <42641846+shati-patel@users.noreply.github.com>
2024-03-27 17:20:50 +0000

    Add unit test for `createModeledMethodKey`

--- extensions/ql-vscode/test/unit-tests/model-editor/modeled-method.test.ts
+++ extensions/ql-vscode/test/unit-tests/model-editor/modeled-method.test.ts
@@ -1,6 +1,10 @@
-import { createNoneModeledMethod } from "../../factories/model-editor/modeled-method-factories";
+import {
+  createNoneModeledMethod,
+  createSummaryModeledMethod,
+} from "../../factories/model-editor/modeled-method-factories";
 import type { ModeledMethod } from "../../../src/model-editor/modeled-method";
 import {
+  createModeledMethodKey,
   modeledMethodSupportsInput,
   modeledMethodSupportsKind,
   modeledMethodSupportsOutput,
@@ -48,5 +52,29 @@ describe("modeledMethodSupportsProvenance", () => {
     if (modeledMethodSupportsProvenance(modeledMethod)) {
       expect(modeledMethod.provenance).not.toBeUndefined();
     }
+  });
+});
+
+describe("createModeledMethodKey", () => {
+  it("should create a key for a modeled method", () => {
+    const modeledMethod = createNoneModeledMethod();
+    const key = createModeledMethodKey(modeledMethod);
+
+    const expectedKey =
+      '{"endpointType":"method","methodName":"createQuery","methodParameters":"(String)","packageName":"org.sql2o","signature":"org.sql2o.Connection#createQuery(String)","type":"none","typeName":"Connection"}';
+
+    expect(key).toBe(expectedKey);
+  });
+
+  it("should check that two modeled methods are the same", () => {
+    const modeledMethod = createSummaryModeledMethod();
+    const key = createModeledMethodKey(modeledMethod);
+
+    const modeledMethod2 = createSummaryModeledMethod();
+    const key2 = createModeledMethodKey(modeledMethod2);
+
+    // Object references are different, but the keys are the same.
+    expect(modeledMethod === modeledMethod2).toBe(false);
+    expect(key === key2).toBe(true);
   });
 });
