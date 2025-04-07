bebde7cec3a54eaf37e2cd576542a18caaacead5
Tuan Nguen <tuan-nguen@github.com>
2025-04-07 12:21:06 +0000

    Tweak ActionButton styles and add comments

--- extensions/ql-vscode/src/view/common/ActionButton/ActionButton.tsx
+++ extensions/ql-vscode/src/view/common/ActionButton/ActionButton.tsx
@@ -3,10 +3,8 @@ import "./ActionButton.css";
 // This is needed because vscode-elements/elements does not implement
 // the same styles for icon buttons as vscode/webview-ui-toolkit
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-export const ActionButton = (props: any) => {
-  return (
-    <button type="button" className="vscode-action-button" {...props}>
-      {props.children}
-    </button>
-  );
-};
+export const ActionButton = (props: any) => (
+  <button type="button" {...props} className="vscode-action-button">
+    {props.children}
+  </button>
+);
