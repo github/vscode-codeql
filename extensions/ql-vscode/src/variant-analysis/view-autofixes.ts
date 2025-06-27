27bdc31ab4e3962cf2ce09dcda2cd5af7e870c52
Jami Cogswell <jcogs33@Jamis-MacBook-Pro.local>
2025-06-27 20:18:55 +0000

    Override query help

--- extensions/ql-vscode/src/variant-analysis/view-autofixes.ts
+++ extensions/ql-vscode/src/variant-analysis/view-autofixes.ts
@@ -8,6 +8,8 @@ import type { CodeQLCliServer } from "../codeql-cli/cli";
 import { pathExists } from "fs-extra";
 import { withProgress, progressUpdate } from "../common/vscode/progress";
 import type { ProgressCallback } from "../common/vscode/progress";
+import { join, dirname, parse } from "path";
+import { tryGetQueryMetadata } from "../codeql-cli/query-metadata";
 
 /**
  * Generates autofixes for the results of a variant analysis.
@@ -27,6 +29,16 @@ export async function viewAutofixesForVariantAnalysisResults(
       // Get the path to the local autofix installation.
       progress(progressUpdate(1, 4, `Checking for local autofix installation`));
       const localAutofixPath = findLocalAutofix();
+
+      // Get the variant analysis with the given id.
+      const variantAnalysis = variantAnalyses.get(variantAnalysisId);
+      if (!variantAnalysis) {
+        throw new Error(`No variant analysis with id: ${variantAnalysisId}`);
+      }
+
+      // Generate the query help and output it to the override directory.
+      progress(progressUpdate(2, 4, `Generating query help override`));
+      await overrideQueryHelp(variantAnalysis, cliServer, localAutofixPath);
 
       // TODO
     },
@@ -52,4 +64,69 @@ function findLocalAutofix(): string {
     throw new Error(`Local autofix path ${localAutofixPath} does not exist.`);
   }
   return localAutofixPath;
+}
+
+/**
+ * Overrides the query help from a given variant analysis
+ * at a location within the `localAutofixPath` directory .
+ */
+async function overrideQueryHelp(
+  variantAnalysis: VariantAnalysis,
+  cliServer: CodeQLCliServer,
+  localAutofixPath: string,
+): Promise<void> {
+  // Get path to the query used by the variant analysis.
+  const queryFilePath = variantAnalysis.query.filePath;
+  if (!(await pathExists(queryFilePath))) {
+    throw new Error(`Query file used by variant analysis not found.`);
+  }
+  const queryFilePathNoExt = join(
+    dirname(queryFilePath),
+    parse(queryFilePath).name,
+  );
+
+  // Get the path to the query help, which may be either a `.qhelp` or a `.md` file.
+  // Note: we assume that the name of the query file is the same as the name of the query help file.
+  const queryHelpFilePathQhelp = `${queryFilePathNoExt}.qhelp`;
+  const queryHelpFilePathMarkdown = `${queryFilePathNoExt}.md`;
+
+  // Set `queryHelpFilePath` to the existing extension type.
+  let queryHelpFilePath: string;
+  if (await pathExists(queryHelpFilePathQhelp)) {
+    queryHelpFilePath = queryHelpFilePathQhelp;
+  } else if (await pathExists(queryHelpFilePathMarkdown)) {
+    queryHelpFilePath = queryHelpFilePathMarkdown;
+  } else {
+    throw new Error(
+      `Could not find query help file at either ${queryHelpFilePathQhelp} or ${queryHelpFilePathMarkdown}. Check that the query help file exists and is named correctly.`,
+    );
+  }
+
+  // Get the query metadata.
+  const metadata = await tryGetQueryMetadata(cliServer, queryFilePath);
+  if (!metadata) {
+    throw new Error(`Could not get query metadata for ${queryFilePath}.`);
+  }
+  // Get the query ID (used for the overridden query help's filename).
+  const queryId = metadata.id;
+  if (!queryId) {
+    throw new Error(`Query metadata for ${queryFilePath} is missing an ID.`);
+  }
+  // Replace `/` with `-` for use with the overridden query help's filename.
+  // Use `replaceAll` since some query IDs have multiple slashes.
+  const queryIdWithDash = queryId.replaceAll("/", "-");
+
+  // Get the path to the output directory for overriding the query help.
+  // Note: the path to this directory may change in the future.
+  const queryHelpOverrideDirectory = join(
+    localAutofixPath,
+    "prompt-templates",
+    "qhelps",
+    `${queryIdWithDash}.md`,
+  );
+
+  await cliServer.generateQueryHelp(
+    queryHelpFilePath,
+    queryHelpOverrideDirectory,
+  );
 }
