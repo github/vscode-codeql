9a8bd792b4611950fdacb60f4fea94462109e862
Jami Cogswell <jcogs33@Jamis-MacBook-Pro.local>
2025-06-27 22:05:56 +0000

    Get full names (owner/repo) of the selected repositories

--- extensions/ql-vscode/src/variant-analysis/view-autofixes.ts
+++ extensions/ql-vscode/src/variant-analysis/view-autofixes.ts
@@ -1,5 +1,8 @@
 import type { RepositoriesFilterSortStateWithIds } from "./shared/variant-analysis-filter-sort";
-import { defaultFilterSortState } from "./shared/variant-analysis-filter-sort";
+import {
+  defaultFilterSortState,
+  filterAndSortRepositoriesWithResults,
+} from "./shared/variant-analysis-filter-sort";
 import type { VariantAnalysis } from "./shared/variant-analysis";
 import type { Credentials } from "../common/authentication";
 import type { NotificationLogger } from "../common/logging";
@@ -10,6 +13,13 @@ import { withProgress, progressUpdate } from "../common/vscode/progress";
 import type { ProgressCallback } from "../common/vscode/progress";
 import { join, dirname, parse } from "path";
 import { tryGetQueryMetadata } from "../codeql-cli/query-metadata";
+import { window as Window } from "vscode";
+
+// Limit to three repos when generating autofixes so not sending
+// too many requests to autofix. Since we only need to validate
+// a handle of autofixes for each query, this should be sufficient.
+// Consider increasing this in the future if needed.
+const MAX_NUM_REPOS: number = 3;
 
 /**
  * Generates autofixes for the results of a variant analysis.
@@ -39,6 +49,12 @@ export async function viewAutofixesForVariantAnalysisResults(
       // Generate the query help and output it to the override directory.
       progress(progressUpdate(2, 4, `Generating query help override`));
       await overrideQueryHelp(variantAnalysis, cliServer, localAutofixPath);
+
+      // Get the full names (nwos) of the selected repositories.
+      const selectedRepoNames = getSelectedRepositoryNames(
+        variantAnalysis,
+        filterSort,
+      );
 
       // TODO
     },
@@ -129,4 +145,39 @@ async function overrideQueryHelp(
     queryHelpFilePath,
     queryHelpOverrideDirectory,
   );
+}
+
+/**
+ * Gets the full names (owner/repo) of the selected
+ * repositories from the given variant analysis while
+ * limiting the number of repositories to `MAX_NUM_REPOS`.
+ */
+function getSelectedRepositoryNames(
+  variantAnalysis: VariantAnalysis,
+  filterSort: RepositoriesFilterSortStateWithIds,
+): string[] {
+  // Get the repositories that were selected by the user.
+  const filteredRepositories = filterAndSortRepositoriesWithResults(
+    variantAnalysis.scannedRepos,
+    filterSort,
+  );
+
+  // Get the full names (owner/repo = nwo) of the selected repos.
+  let fullNames = filteredRepositories
+    ?.filter((a) => a.resultCount && a.resultCount > 0)
+    .map((a) => a.repository.fullName);
+  if (!fullNames || fullNames.length === 0) {
+    throw new Error("No repositories with results found.");
+  }
+
+  // Limit to MAX_NUM_REPOS by slicing the array,
+  // and inform the user about the limit.
+  if (fullNames.length > MAX_NUM_REPOS) {
+    fullNames = fullNames.slice(0, MAX_NUM_REPOS);
+    void Window.showInformationMessage(
+      `Only the first ${MAX_NUM_REPOS} repos (${fullNames.join(", ")}) will be included in the Autofix results.`,
+    );
+  }
+
+  return fullNames;
 }
