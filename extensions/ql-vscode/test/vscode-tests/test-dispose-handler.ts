d20f8a650caa1b0e206efdbdd2ee5f7be17b467d
Robert <robertbrignull@github.com>
2025-08-11 11:36:12 +0000

    Add isTreeView type checker function

--- extensions/ql-vscode/test/vscode-tests/test-dispose-handler.ts
+++ extensions/ql-vscode/test/vscode-tests/test-dispose-handler.ts
@@ -1,13 +1,19 @@
-import type { Disposable } from "vscode";
+import type { Disposable, TreeView } from "vscode";
 import { DisposableObject } from "../../src/common/disposable-object";
 
+function isTreeView(obj: unknown): obj is TreeView<unknown> {
+  return (
+    typeof obj === "object" &&
+    obj !== null &&
+    "onDidExpandElement" in obj &&
+    "onDidCollapseElement" in obj &&
+    "reveal" in obj
+  );
+}
+
 export function testDisposeHandler(disposable: Disposable) {
-  if (
-    (disposable as any).onDidExpandElement &&
-    (disposable as any).onDidCollapseElement &&
-    (disposable as any).reveal
-  ) {
-    // This looks like a treeViewer. Don't dispose
+  if (isTreeView(disposable)) {
+    // This looks like a TreeView. Don't dispose
     return;
   }
   if (disposable instanceof DisposableObject) {
