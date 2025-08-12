0a55f024a9a93226bd3d4b62f4643cec2fa6402c
Charis Kyriakou <charisk@users.noreply.github.com>
2025-08-12 10:31:24 +0000

    Remove support for CodeQL CLI versions older than v2.19.4

--- extensions/ql-vscode/src/codeql-cli/cli.ts
+++ extensions/ql-vscode/src/codeql-cli/cli.ts
@@ -1902,7 +1902,7 @@ function shouldDebugCliServer() {
 export class CliVersionConstraint {
   // The oldest version of the CLI that we support. This is used to determine
   // whether to show a warning about the CLI being too old on startup.
-  public static OLDEST_SUPPORTED_CLI_VERSION = new SemVer("2.18.4");
+  public static OLDEST_SUPPORTED_CLI_VERSION = new SemVer("2.19.4");
 
   constructor(private readonly cli: CodeQLCliServer) {
     /**/
