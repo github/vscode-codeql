github/vscode-codeql

commit 459d6a9bc8724b45b99ab4b427c24032c16ed13f

Add command for running a query suite
This uses a new query-server command for running multiple queries, so
that a single evaluator log will be produced for the entire run.

To avoid too much code duplication, I have updated a lot of the code
paths involved in running local queries to work with multiple query
paths. This also required some refactoring to explicitly associate an output basename (used to
produce the .bqrs, .csv, etc. paths) with each query, where before those output filenames were
hard-coded.

extensions/ql-vscode/src/local-queries/local-query-run.ts
@@ -4,7 +4,7 @@ import {
   showAndLogExceptionWithTelemetry,
   showAndLogWarningMessage,
 } from "../common/logging";
-import type { CoreQueryResults } from "../query-server";
+import type { CoreQueryResult, CoreQueryResults } from "../query-server";
 import type { QueryHistoryManager } from "../query-history/query-history-manager";
 import type { DatabaseItem } from "../databases/local-databases";
 import type {
@@ -29,7 +29,7 @@ import type { Disposable } from "../common/disposable-object";
 import type { ProgressCallback } from "../common/vscode/progress";
 import { progressUpdate } from "../common/vscode/progress";
 
-function formatResultMessage(result: CoreQueryResults): string {
+function formatResultMessage(result: CoreQueryResult): string {
   switch (result.resultType) {
     case QueryResultType.CANCELLATION:
       return `cancelled after ${Math.round(
@@ -86,7 +86,9 @@ export class LocalQueryRun {
     progress: ProgressCallback,
   ): Promise<void> {
     const evalLogPaths = await this.summarizeEvalLog(
-      results.resultType,
+      Array.from(results.results.values()).every(
+        (result) => result.resultType === QueryResultType.SUCCESS,
+      ),
       this.outputDir,
       this.logger,
       progress,
@@ -95,9 +97,12 @@ export class LocalQueryRun {
       this.queryInfo.setEvaluatorLogPaths(evalLogPaths);
     }
     progress(progressUpdate(1, 4, "Getting completed query info"));
-    const queryWithResults = await this.getCompletedQueryInfo(results);
+    const queriesWithResults = await this.getCompletedQueryInfo(results);
     progress(progressUpdate(2, 4, "Updating query history"));
-    this.queryHistoryManager.completeQuery(this.queryInfo, queryWithResults);
+    this.queryHistoryManager.completeQueries(
+      this.queryInfo,
+      queriesWithResults,
+    );
     progress(progressUpdate(3, 4, "Showing results"));
     await this.localQueries.showResultsForCompletedQuery(
       this.queryInfo as CompletedLocalQueryInfo,
@@ -116,7 +121,7 @@ export class LocalQueryRun {
    */
   public async fail(err: Error): Promise<void> {
     const evalLogPaths = await this.summarizeEvalLog(
-      QueryResultType.OTHER_ERROR,
+      false,
       this.outputDir,
       this.logger,
       (_) => {},
@@ -136,7 +141,7 @@ export class LocalQueryRun {
    * Generate summaries of the structured evaluator log.
    */
   private async summarizeEvalLog(
-    resultType: QueryResultType,
+    runSuccessful: boolean,
     outputDir: QueryOutputDir,
     logger: BaseLogger,
     progress: ProgressCallback,
@@ -152,7 +157,7 @@ export class LocalQueryRun {
       }
     } else {
       // Raw evaluator log was not found. Notify the user, unless we know why it wasn't found.
-      if (resultType === QueryResultType.SUCCESS) {
+      if (runSuccessful) {
         void showAndLogWarningMessage(
           extLogger,
           `Failed to write structured evaluator log to ${outputDir.evalLogPath}.`,
@@ -168,41 +173,43 @@ export class LocalQueryRun {
   }
 
   /**
-   * Gets a `QueryWithResults` containing information about the evaluation of the query and its
+   * Gets a `QueryWithResults` containing information about the evaluation of the queries and their
    * result, in the form expected by the query history UI.
    */
   private async getCompletedQueryInfo(
     results: CoreQueryResults,
-  ): Promise<QueryWithResults> {
-    // Read the query metadata if possible, to use in the UI.
-    const metadata = await tryGetQueryMetadata(
-      this.cliServer,
-      this.queryInfo.initialInfo.queryPath,
-    );
-    const query = new QueryEvaluationInfo(
-      this.outputDir.querySaveDir,
-      this.dbItem.databaseUri.fsPath,
-      await this.dbItem.hasMetadataFile(),
-      this.queryInfo.initialInfo.quickEvalPosition,
-      metadata,
-    );
-
-    if (results.resultType !== QueryResultType.SUCCESS) {
-      const message = results.message
-        ? redactableError`Failed to run query: ${results.message}`
-        : redactableError`Failed to run query`;
-      void showAndLogExceptionWithTelemetry(
-        extLogger,
-        telemetryListener,
-        message,
+  ): Promise<QueryWithResults[]> {
+    const infos: QueryWithResults[] = [];
+    for (const [queryPath, result] of results.results) {
+      // Read the query metadata if possible, to use in the UI.
+      const metadata = await tryGetQueryMetadata(this.cliServer, queryPath);
+      const query = new QueryEvaluationInfo(
+        this.outputDir.querySaveDir,
+        result.outputBaseName,
+        this.dbItem.databaseUri.fsPath,
+        await this.dbItem.hasMetadataFile(),
+        undefined,
+        metadata,
       );
+
+      if (result.resultType !== QueryResultType.SUCCESS) {
+        const message = result.message
+          ? redactableError`Failed to run query: ${result.message}`
+          : redactableError`Failed to run query`;
+        void showAndLogExceptionWithTelemetry(
+          extLogger,
+          telemetryListener,
+          message,
+        );
+      }
+      const message = formatResultMessage(result);
+      const successful = result.resultType === QueryResultType.SUCCESS;
+      infos.push({
+        query,
+        message,
+        successful,
+      });
     }
-    const message = formatResultMessage(results);
-    const successful = results.resultType === QueryResultType.SUCCESS;
-    return {
-      query,
-      message,
-      successful,
-    };
+    return infos;
   }
 }

extensions/ql-vscode/src/codeql-cli/cli-version.ts
@@ -13,6 +13,7 @@ export interface CliFeatures {
   featuresInVersionResult?: boolean;
   mrvaPackCreate?: boolean;
   generateSummarySymbolMap?: boolean;
+  queryServerRunQueries?: boolean;
 }
 
 export interface VersionAndFeatures {

extensions/ql-vscode/src/common/commands.ts
@@ -138,6 +138,7 @@ export type LocalQueryCommands = {
   "codeQLQueries.createQuery": () => Promise<void>;
   "codeQL.runLocalQueryFromFileTab": (uri: Uri) => Promise<void>;
   "codeQL.runQueries": ExplorerSelectionCommandFunction<Uri>;
+  "codeQL.runQuerySuite": ExplorerSelectionCommandFunction<Uri>;
   "codeQL.quickEval": (uri: Uri) => Promise<void>;
   "codeQL.quickEvalCount": (uri: Uri) => Promise<void>;
   "codeQL.quickEvalContextEditor": (uri: Uri) => Promise<void>;

extensions/ql-vscode/src/compare/compare-view.ts
@@ -70,22 +70,20 @@ export class CompareView extends AbstractWebview<
     selectedResultSetName?: string,
   ) {
     const [fromSchemas, toSchemas] = await Promise.all([
-      this.cliServer.bqrsInfo(
-        from.completedQuery.query.resultsPaths.resultsPath,
-      ),
-      this.cliServer.bqrsInfo(to.completedQuery.query.resultsPaths.resultsPath),
+      this.cliServer.bqrsInfo(from.completedQuery.query.resultsPath),
+      this.cliServer.bqrsInfo(to.completedQuery.query.resultsPath),
     ]);
 
     const [fromSchemaNames, toSchemaNames] = await Promise.all([
       getResultSetNames(
         fromSchemas,
         from.completedQuery.query.metadata,
-        from.completedQuery.query.resultsPaths.interpretedResultsPath,
+        from.completedQuery.query.interpretedResultsPath,
       ),
       getResultSetNames(
         toSchemas,
         to.completedQuery.query.metadata,
-        to.completedQuery.query.resultsPaths.interpretedResultsPath,
+        to.completedQuery.query.interpretedResultsPath,
       ),
     ]);
 
@@ -101,15 +99,14 @@ export class CompareView extends AbstractWebview<
         schemaNames: fromSchemaNames,
         metadata: from.completedQuery.query.metadata,
         interpretedResultsPath:
-          from.completedQuery.query.resultsPaths.interpretedResultsPath,
+          from.completedQuery.query.interpretedResultsPath,
       },
       to,
       toInfo: {
         schemas: toSchemas,
         schemaNames: toSchemaNames,
         metadata: to.completedQuery.query.metadata,
-        interpretedResultsPath:
-          to.completedQuery.query.resultsPaths.interpretedResultsPath,
+        interpretedResultsPath: to.completedQuery.query.interpretedResultsPath,
       },
       commonResultSetNames,
     };
@@ -392,12 +389,12 @@ export class CompareView extends AbstractWebview<
       this.getResultSet(
         fromInfo.schemas,
         fromResultSetName,
-        from.completedQuery.query.resultsPaths.resultsPath,
+        from.completedQuery.query.resultsPath,
       ),
       this.getResultSet(
         toInfo.schemas,
         toResultSetName,
-        to.completedQuery.query.resultsPaths.resultsPath,
+        to.completedQuery.query.resultsPath,
       ),
     ]);
 

extensions/ql-vscode/src/compare/interpreted-results.ts
@@ -36,11 +36,9 @@ export async function compareInterpretedResults(
 
   const [fromResultSet, toResultSet, sourceLocationPrefix] = await Promise.all([
     getInterpretedResults(
-      fromQuery.completedQuery.query.resultsPaths.interpretedResultsPath,
+      fromQuery.completedQuery.query.interpretedResultsPath,
     ),
-    getInterpretedResults(
-      toQuery.completedQuery.query.resultsPaths.interpretedResultsPath,
-    ),
+    getInterpretedResults(toQuery.completedQuery.query.interpretedResultsPath),
     database.getSourceLocationPrefix(cliServer),
   ]);
 

extensions/ql-vscode/src/debugger/debug-protocol.ts
@@ -39,6 +39,7 @@ export interface EvaluationCompletedEvent extends Event {
     resultType: QueryResultType;
     message: string | undefined;
     evaluationTime: number;
+    outputBaseName: string;
   };
 }
 

extensions/ql-vscode/src/debugger/debug-session.ts
@@ -16,7 +16,7 @@ import type { BaseLogger, LogOptions } from "../common/logging";
 import { queryServerLogger } from "../common/logging/vscode";
 import { QueryResultType } from "../query-server/messages";
 import type {
-  CoreQueryResults,
+  CoreQueryResult,
   CoreQueryRun,
   QueryRunner,
 } from "../query-server";
@@ -25,6 +25,7 @@ import type * as CodeQLProtocol from "./debug-protocol";
 import type { QuickEvalContext } from "../run-queries-shared";
 import { getErrorMessage } from "../common/helpers-pure";
 import { DisposableObject } from "../common/disposable-object";
+import { basename } from "path";
 
 // More complete implementations of `Event` for certain events, because the classes from
 // `@vscode/debugadapter` make it more difficult to provide some of the message values.
@@ -107,9 +108,9 @@ class EvaluationCompletedEvent
   public readonly event = "codeql-evaluation-completed";
   public readonly body: CodeQLProtocol.EvaluationCompletedEvent["body"];
 
-  constructor(results: CoreQueryResults) {
+  constructor(result: CoreQueryResult) {
     super("codeql-evaluation-completed");
-    this.body = results;
+    this.body = result;
   }
 }
 
@@ -143,6 +144,7 @@ const QUERY_THREAD_NAME = "Evaluation thread";
 class RunningQuery extends DisposableObject {
   private readonly tokenSource = this.push(new CancellationTokenSource());
   public readonly queryRun: CoreQueryRun;
+  private readonly queryPath: string;
 
   public constructor(
     queryRunner: QueryRunner,
@@ -154,21 +156,25 @@ class RunningQuery extends DisposableObject {
   ) {
     super();
 
+    this.queryPath = config.query;
     // Create the query run, which will give us some information about the query even before the
     // evaluation has completed.
     this.queryRun = queryRunner.createQueryRun(
       config.database,
-      {
-        queryPath: config.query,
-        quickEvalPosition: quickEvalContext?.quickEvalPosition,
-        quickEvalCountOnly: quickEvalContext?.quickEvalCount,
-      },
+      [
+        {
+          queryPath: this.queryPath,
+          outputBaseName: "results",
+          quickEvalPosition: quickEvalContext?.quickEvalPosition,
+          quickEvalCountOnly: quickEvalContext?.quickEvalCount,
+        },
+      ],
       true,
       config.additionalPacks,
       config.extensionPacks,
       config.additionalRunQueryArgs,
       queryStorageDir,
-      undefined,
+      basename(config.query),
       undefined,
     );
   }
@@ -208,7 +214,7 @@ class RunningQuery extends DisposableObject {
       progressStart.body.cancellable = true;
       this.sendEvent(progressStart);
       try {
-        return await this.queryRun.evaluate(
+        const completedQuery = await this.queryRun.evaluate(
           (p) => {
             const progressUpdate = new ProgressUpdateEvent(
               this.queryRun.id,
@@ -220,6 +226,14 @@ class RunningQuery extends DisposableObject {
           this.tokenSource.token,
           this.logger,
         );
+        return (
+          completedQuery.results.get(this.queryPath) ?? {
+            resultType: QueryResultType.OTHER_ERROR,
+            message: "Missing query results",
+            evaluationTime: 0,
+            outputBaseName: "unknown",
+          }
+        );
       } finally {
         this.sendEvent(new ProgressEndEvent(this.queryRun.id));
       }
@@ -229,6 +243,7 @@ class RunningQuery extends DisposableObject {
         resultType: QueryResultType.OTHER_ERROR,
         message,
         evaluationTime: 0,
+        outputBaseName: "unknown",
       };
     }
   }

extensions/ql-vscode/src/debugger/debugger-ui.ts
@@ -8,7 +8,7 @@ import { debug, Uri, CancellationTokenSource } from "vscode";
 import type { DebuggerCommands } from "../common/commands";
 import type { DatabaseManager } from "../databases/local-databases";
 import { DisposableObject } from "../common/disposable-object";
-import type { CoreQueryResults } from "../query-server";
+import type { CoreQueryResult } from "../query-server";
 import {
   getQuickEvalContext,
   saveBeforeStart,
@@ -134,8 +134,15 @@ class QLDebugAdapterTracker
     body: EvaluationCompletedEvent["body"],
   ): Promise<void> {
     if (this.localQueryRun !== undefined) {
-      const results: CoreQueryResults = body;
-      await this.localQueryRun.complete(results, (_) => {});
+      const results: CoreQueryResult = body;
+      await this.localQueryRun.complete(
+        {
+          results: new Map<string, CoreQueryResult>([
+            [this.configuration.query, results],
+          ]),
+        },
+        (_) => {},
+      );
       this.localQueryRun = undefined;
     }
   }

commit 6053663b00425a151bc0b4c89ec4d081532cc5d7

Don't attempt to interpret results when running quick-eval
459d6a9b introduced a bug where quickEvalPosition -- which indicates
that the query was spawned via quick-eval -- was not being passed
through in the completed query info. This meant the extension tried to
interpret the results with the metadata from the enclosing file, which
would then fail if it was a .ql file with metadata.

extensions/ql-vscode/src/local-queries/local-query-run.ts
@@ -188,7 +188,7 @@ export class LocalQueryRun {
         result.outputBaseName,
         this.dbItem.databaseUri.fsPath,
         await this.dbItem.hasMetadataFile(),
-        undefined,
+        this.queryInfo.initialInfo.quickEvalPosition,
         metadata,
       );
 