9bb787df81611d51d5e20ea2b2d08bf7d73688c4
Nick Rolfe <nickrolfe@github.com>
2025-11-07 13:14:40 +0000

    Fix compilation error after updating ts-unused-exports

--- extensions/ql-vscode/scripts/find-deadcode.ts
+++ extensions/ql-vscode/scripts/find-deadcode.ts
@@ -1,5 +1,5 @@
 import { basename, join, relative, resolve } from "path";
-import analyzeTsConfig from "ts-unused-exports";
+import { analyzeTsConfig } from "ts-unused-exports";
 import { containsPath, pathsEqual } from "../src/common/files";
 import { exit } from "process";
 
@@ -26,7 +26,9 @@ function main() {
   const result = analyzeTsConfig("tsconfig.deadcode.json");
   let foundUnusedExports = false;
 
-  for (const [filepath, exportNameAndLocations] of Object.entries(result)) {
+  for (const [filepath, exportNameAndLocations] of Object.entries(
+    result.unusedExports,
+  )) {
     const relativeFilepath = relative(repositoryRoot, filepath);
 
     if (ignoreFile(relativeFilepath)) {
